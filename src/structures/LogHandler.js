880efa2378775757de00ce935e815f16843e5936
muchnameless <muchnameless@gmail.com>
2021-02-16 00:15:06 +0100

    fix(logToFile): replace forbidden chars in filename

--- src/structures/LogHandler.js
+++ src/structures/LogHandler.js
@@ -108,7 +108,7 @@ class LogHandler {
 	async _createLogBufferFolder() {
 		return fs.mkdir(this.logBufferPath).then(
 			() => logger.debug('[LOG BUFFER]: created \'log_buffer\' folder'),
-			() => null,
+			() => null, // rejects if folder already exists
 		);
 	}
 
@@ -120,10 +120,11 @@ class LogHandler {
 		try {
 			await this._createLogBufferFolder();
 			await fs.writeFile(
-				path.join(this.logBufferPath, `${new Date().toLocaleString('de-DE', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' })}_${SnowflakeUtil.generate()}`),
+				path.join(this.logBufferPath, `${new Date().toLocaleString('de-DE', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' }).replace(', ', '_').replace(/:/g, '.')}_${SnowflakeUtil.generate()}`),
 				data,
 			);
 		} catch (error) {
+			logger.error(error);
 			logger.error(`[LOG TO FILE]: ${error.name}: ${error.message}`);
 		}
 	}
