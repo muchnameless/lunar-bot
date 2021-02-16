a444d077b7a1b4e470b463102cabef85e4246f69
muchnameless <muchnameless@gmail.com>
2021-02-16 15:07:15 +0100

    feat(ChatBridge#bot): error event

---
+++ src/structures/chat_bridge/events/error.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const logger = require('../../../functions/logger');
+
+/**
+ * @param {import('../../LunarClient')} client
+ * @param {import('mineflayer').Bot} bot
+ */
+module.exports = (client, bot, error) => {
+	logger.error('[CHATBRIDGE ERROR]:', error);
+};
