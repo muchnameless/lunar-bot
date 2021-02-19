190d67f1fc65fe506ab986c50d19e23da0a0f9f3
muchnameless <muchnameless@gmail.com>
2021-02-19 19:00:31 +0100

    feat(ChatBridge#error): don't reconnect on 'Invalid credentials'

--- src/structures/chat_bridge/bot_events/error.js
+++ src/structures/chat_bridge/bot_events/error.js
@@ -8,5 +8,12 @@ const logger = require('../../../functions/logger');
  */
 module.exports = (chatBridge, error) => {
 	logger.error('[CHATBRIDGE ERROR]:', error);
+
+	if (error.message.includes('Invalid credentials')) {
+		chatBridge.ready = false;
+		chatBridge.criticalError = true;
+		return logger.error('[CHATBRIDGE]: invalid credentials detected');
+	}
+
 	chatBridge.reconnect();
 };
