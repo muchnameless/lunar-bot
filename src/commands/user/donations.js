c563d322bc646dcfb8c6f5766a407982765c488b
muchnameless <muchnameless@gmail.com>
2021-02-25 04:01:50 +0100

    feat(donations): new command

---
+++ src/commands/user/donations.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const { MessageEmbed } = require('discord.js');
+const { stripIndent } = require('common-tags');
+const Command = require('../../structures/commands/Command');
+const mojang = require('../../api/mojang');
+const logger = require('../../functions/logger');
+
+
+module.exports = class MyCommand extends Command {
+	constructor(data) {
+		super(data, {
+			aliases: [],
+			description: 'donations leaderboard',
+			args: false,
+			usage: '',
+			cooldown: 0,
+		});
+	}
+
+	/**
+	 * execute the command
+	 * @param {import('../../structures/LunarClient')} client
+	 * @param {import('../../structures/database/ConfigHandler')} config
+	 * @param {import('../../structures/extensions/Message')} message message that triggered the command
+	 * @param {string[]} args command arguments
+	 * @param {string[]} flags command flags
+	 * @param {string[]} rawArgs arguments and flags
+	 */
+	async run(client, config, message, args, flags, rawArgs) {
+		// aquire donations from db
+		const donations = await client.db.models.Transaction.findAll({
+			where: { type: 'donation' },
+		});
+
+		// construct { donator: amount } object
+		const reduced = Object.fromEntries([ ...new Set(donations.map(d => d.from)) ].map(x => [ x, 0 ]));
+
+		// fill said object
+		for (const { from, amount } of donations) {
+			reduced[from] += amount;
+		}
+
+		// transform and prettify data
+		let playerList = '';
+		let totalAmount = 0;
+
+		await Promise.all([ ...Object.entries(reduced) ].map(async ([ minecraftUUID, amount ], index) => {
+			const IGN = client.players.cache.get(minecraftUUID)?.ign ?? (await mojang.getName(IGN).catch(logger.error)) ?? minecraftUUID;
+
+			playerList += '\n' + stripIndent`
+				#${`${index + 1}`.padStart(3, '0')} : ${IGN}
+					 > ${client.formatNumber(amount)}
+			`;
+
+			totalAmount += amount;
+		}));
+
+		// create and send embed
+		message.reply(new MessageEmbed()
+			.setColor(config.get('EMBED_BLUE'))
+			.setTitle('Guild Donations')
+			.setDescription(stripIndent`
+				Total: ${client.formatNumber(totalAmount)}
+				\`\`\`ada${playerList}\`\`\`
+			`)
+			.setTimestamp(),
+		);
+	}
+};
