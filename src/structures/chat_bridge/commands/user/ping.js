eb3da73c43726524598f57f6b4e6e9da89eb5a0c
muchnameless <muchnameless@gmail.com>
2021-02-26 14:49:13 +0100

    feat(ping): initial commit

---
+++ src/structures/chat_bridge/commands/user/ping.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const IngameCommand = require('../../IngameCommand');
+const logger = require('../../../../functions/logger');
+
+
+module.exports = class PingCommand extends IngameCommand {
+	constructor(data) {
+		super(data, {
+			aliases: [ 'o/', '( ﾟ◡ﾟ)/' ],
+			description: 'ping the bot',
+			args: false,
+			usage: '',
+			cooldown: 0,
+		});
+	}
+
+	/**
+	 * execute the command
+	 * @param {import('../../ChatBridge')} chatBridge
+	 * @param {import('../../../LunarClient')} client
+	 * @param {import('../../../database/ConfigHandler')} config
+	 * @param {import('../../HypixelMessage')} message message that triggered the command
+	 * @param {string[]} args command arguments
+	 * @param {string[]} flags command flags
+	 * @param {string[]} rawArgs arguments and flags
+	 */
+	async run(chatBridge, client, config, message, args, flags, rawArgs) {
+		message.reply('o/');
+	}
+};
