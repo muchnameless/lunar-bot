9532b419f3cb5945dbbabe886d076144d62e5596
muchnameless <muchnameless@gmail.com>
2021-02-26 19:02:16 +0100

    feat(commands#unload): initial commit

---
+++ src/commands/owner/unload.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Command = require('../../structures/commands/Command');
+const logger = require('../../functions/logger');
+
+
+module.exports = class UnloadCommand extends Command {
+	constructor(data) {
+		super(data, {
+			aliases: [],
+			description: 'unload a command',
+			args: true,
+			usage: '[`command name` to unload]',
+			cooldown: 0,
+		});
+	}
+
+	/**
+	 * execute the command
+	 * @param {import('../../structures/LunarClient')} client
+	 * @param {import('../../structures/database/ConfigHandler')} config
+	 * @param {import('../../structures/extensions/Message')} message message that triggered the command
+	 * @param {string[]} args command arguments
+	 * @param {string[]} flags command flags
+	 * @param {string[]} rawArgs arguments and flags
+	 */
+	async run(client, config, message, args, flags, rawArgs) {
+		const INPUT = args[0].toLowerCase();
+		const command = client.commands.getByName(INPUT);
+
+		if (!command) return message.reply(`no command with the name or alias \`${INPUT}\` found.`);
+
+		command.unload();
+
+		message.reply(`command \`${command.name}\` was unloaded successfully.`);
+	}
+};
---
+++ src/structures/chat_bridge/commands/owner/unload.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const IngameCommand = require('../../IngameCommand');
+const logger = require('../../../../functions/logger');
+
+
+module.exports = class MyCommand extends IngameCommand {
+	constructor(data) {
+		super(data, {
+			aliases: [],
+			description: 'unload a command',
+			args: true,
+			usage: '[`command name` to unload]',
+			cooldown: 0,
+		});
+	}
+
+	/**
+	 * execute the command
+	 * @param {import('../../../LunarClient')} client
+	 * @param {import('../../../database/ConfigHandler')} config
+	 * @param {import('../../HypixelMessage')} message message that triggered the command
+	 * @param {string[]} args command arguments
+	 * @param {string[]} flags command flags
+	 * @param {string[]} rawArgs arguments and flags
+	 */
+	async run(client, config, message, args, flags, rawArgs) {
+		const INPUT = args[0].toLowerCase();
+		const command = client.chatBridges.commands.getByName(INPUT);
+
+		if (!command) return message.reply(`no command with the name or alias '${INPUT}' found`);
+
+		command.unload();
+
+		message.reply(`command '${command.name}' was unloaded successfully`);
+	}
+};
