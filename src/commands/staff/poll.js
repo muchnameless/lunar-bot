2d080226a1cea6e6979eec8c6eb3c2fcc34f5d51
muchnameless <muchnameless@gmail.com>
2021-02-27 03:27:41 +0100

    feat(polls)

---
+++ src/commands/staff/poll.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const createPoll = require('../../functions/createPoll');
+const Command = require('../../structures/commands/Command');
+const logger = require('../../functions/logger');
+
+
+module.exports = class PollCommand extends Command {
+	constructor(data) {
+		super(data, {
+			aliases: [ 'polls' ],
+			description: '',
+			args: false,
+			usage: '<\'time\'> ["option1" "option2" ...]',
+			cooldown: 30,
+		});
+	}
+
+	/**
+	 * execute the command
+	 * @param {import('../../structures/LunarClient')} client
+	 * @param {import('../../structures/database/ConfigHandler')} config
+	 * @param {import('../../structures/extensions/Message')} message message that triggered the command
+	 * @param {string[]} args command arguments
+	 * @param {string[]} flags command flags
+	 * @param {string[]} rawArgs arguments and flags
+	 */
+	async run(client, config, message, args, flags, rawArgs) {
+		const chatBridge = message.author.hypixelGuild?.chatBridge;
+
+		if (!chatBridge) return message.reply(`unable to find a chat bridge for ${message.author.player?.guildName ?? 'unknown player'}`);
+
+		createPoll(chatBridge, message, args, message.author.player.ign);
+	}
+};
---
+++ src/functions/createPoll.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const { MessageEmbed } = require('discord.js');
+const ms = require('ms');
+const { messageTypes: { GUILD, WHISPER } } = require('../constants/chatBridge');
+const logger = require('./logger');
+
+
+/**
+ * create a poll for both ingame chat and the chatBridge channel
+ * @param {import('../structures/chat_bridge/ChatBridge')} chatBridge
+ * @param {import('../structures/chat_bridge/HypixelMessage')|import('../structures/extensions/Message')}
+ * @param {string[]} args
+ * @param {string} ign
+ */
+module.exports = async (chatBridge, message, args, ign) => {
+	const duration = ms(args[0]) ?? 60_000;
+	const startingIndex = message.content.indexOf('"');
+
+	// no '"' found
+	if (startingIndex === -1) return message.reply('specify poll options to vote for');
+
+	const options = message.content
+		.slice(startingIndex)
+		.split('"')
+		.map(x => x.trim())
+		.filter(x => x.length)
+		.map(x => ({ option: x, votes: new Set() }));
+	const optionsCount = options.length;
+	const ingameMessages = chatBridge.awaitMessages(
+		msg => msg.type === GUILD || msg.type === WHISPER,
+		{ time: duration },
+	);
+	const discordMessages = chatBridge.channel.awaitMessages(
+		msg => msg.isUserMessage,
+		{ time: duration },
+	);
+
+	// post message to both chats
+	chatBridge.broadcast(`poll by ${ign}: type a number to vote\n${options.map(({ option }, index) => `${index + 1}: ${option}`).join('\n')}`);
+
+	// aquire ingame votes
+	for (const msg of await ingameMessages) {
+		if (/\D/.test(msg.content)) continue;
+
+		const votedFor = Number(msg.content);
+
+		if (votedFor < 1 || votedFor > optionsCount) continue;
+
+		options[votedFor - 1].votes.add(msg.player?.minecraftUUID ?? msg.author.ign);
+	}
+
+	// aquire discord votes
+	for (const msg of (await discordMessages).values()) {
+		if (/\D/.test(msg.content)) continue;
+
+		const votedFor = Number(msg.content);
+
+		if (votedFor < 1 || votedFor > optionsCount) continue;
+
+		options[votedFor - 1].votes.add(msg.author.player?.minecraftUUID ?? msg.author.id);
+	}
+
+	// count votes and sort options by them
+	const result = options
+		.map(({ option, votes }) => ({ option, votes: votes.size }))
+		.sort((a, b) => b.votes - a.votes);
+	const TOTAL_VOTES = result.reduce((acc, { votes }) => acc + votes, 0);
+	const resultString = result.map(({ option, votes }, index) => `#${index + 1}: ${option} (${Math.round(votes / TOTAL_VOTES * 100) || 0}%, ${votes} vote${votes === 1 ? '' : 's'})`);
+
+	// reply with result
+	chatBridge.channel.send(new MessageEmbed()
+		.setColor(chatBridge.client.config.get('EMBED_BLUE'))
+		.setTitle(`Poll by ${ign} - Result`)
+		.setDescription(resultString.join('\n\n'))
+		.setTimestamp(),
+	);
+
+	// chatBridge.gchat(resultString.join('; \n\n'));
+	resultString.forEach(res => chatBridge.gchat(res));
+};
