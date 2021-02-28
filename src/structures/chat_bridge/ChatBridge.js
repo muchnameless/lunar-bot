d967586ebb7d10829dcea9ad06f43a1138bd68e2
muchnameless <muchnameless@gmail.com>
2021-02-28 01:31:30 +0100

    feat(ChatBridge#command)

--- src/structures/chat_bridge/ChatBridge.js
+++ src/structures/chat_bridge/ChatBridge.js
@@ -365,6 +365,14 @@ class ChatBridge extends EventEmitter {
 	}
 
 	/**
+	 * sends the message as a command to ingame chat, without altering it
+	 * @param {string} message
+	 */
+	async command(message) {
+		return this.queueForMinecraftChat(trim(`/${message}`, this.maxMessageLength - 1));
+	}
+
+	/**
 	 * send a message to the ingame chat, without changing it, 600 ms queue cooldown
 	 * @param {string} message
 	 */
