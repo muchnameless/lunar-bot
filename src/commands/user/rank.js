865560dd13e2f838a0df3ad6562af57625e4d604
muchnameless <muchnameless@gmail.com>
2021-03-01 00:05:28 +0100

    feat(commands): rank

---
+++ src/commands/user/rank.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Command = require('../../structures/commands/Command');
+const logger = require('../../functions/logger');
+
+
+module.exports = class RankCommand extends Command {
+	constructor(data) {
+		super(data, {
+			aliases: [ 'request' ],
+			description: 'request a guild rank',
+			args: true,
+			usage: '[rank name]',
+			cooldown: 1,
+		});
+	}
+
+	/**
+	 * execute the command
+	 * @param {import('../../structures/LunarClient')} client
+	 * @param {import('../../structures/database/managers/ConfigManager')} config
+	 * @param {import('../../structures/extensions/Message')} message message that triggered the command
+	 * @param {string[]} args command arguments
+	 * @param {string[]} flags command flags
+	 * @param {string[]} rawArgs arguments and flags
+	 */
+	async run(client, config, message, args, flags, rawArgs) {
+		(message.author.player?.guild ?? client.hypixelGuilds.cache.first()).handleRankRequestMessage(message);
+	}
+};
