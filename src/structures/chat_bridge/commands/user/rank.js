3cdca000c79cf967658a1c24b10e4d1beb98e89f
muchnameless <muchnameless@gmail.com>
2021-02-28 22:20:13 +0100

    feat(ChatBridge#commands): rank command

---
+++ src/structures/chat_bridge/commands/user/rank.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const handleRankRequest = require('../../functions/handleRankRequest');
+const IngameCommand = require('../../IngameCommand');
+const logger = require('../../../../functions/logger');
+
+
+module.exports = class RankCommand extends IngameCommand {
+	constructor(data) {
+		super(data, {
+			aliases: [ 'request' ],
+			description: 'request a guild rank',
+			args: false,
+			usage: '',
+			cooldown: 0,
+		});
+	}
+
+	/**
+	 * execute the command
+	 * @param {import('../../../LunarClient')} client
+	 * @param {import('../../../database/managers/ConfigManager')} config
+	 * @param {import('../../HypixelMessage')} message message that triggered the command
+	 * @param {string[]} args command arguments
+	 * @param {string[]} flags command flags
+	 * @param {string[]} rawArgs arguments and flags
+	 */
+	async run(client, config, message, args, flags, rawArgs) {
+		handleRankRequest(message);
+	}
+};
