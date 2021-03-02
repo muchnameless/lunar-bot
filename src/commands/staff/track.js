af3567ad3becedbc86e33d4f82b7980edc589357
muchnameless <muchnameless@gmail.com>
2021-03-02 01:19:07 +0100

    feat(track): types

--- src/commands/staff/track.js
+++ src/commands/staff/track.js
@@ -2,6 +2,10 @@
 
 const { MessageEmbed, MessageAttachment } = require('discord.js');
 const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
+const { oneLine, stripIndents } = require('common-tags');
+const { autocorrect, upperCaseFirstChar } = require('../../functions/util');
+const { XP_TYPES } = require('../../constants/database');
+const { SKILLS, COSMETIC_SKILLS, SLAYERS, DUNGEON_TYPES, DUNGEON_CLASSES } = require('../../constants/skyblock');
 const Command = require('../../structures/commands/Command');
 const logger = require('../../functions/logger');
 
@@ -12,8 +16,17 @@ module.exports = class TracklistCommand extends Command {
 			aliases: [],
 			description: 'gained weight from members below reqs',
 			args: false,
-			usage: '',
-			cooldown: 0,
+			usage: () => stripIndents`
+				<\`IGN\`|\`@mention\`> <\`type\`>
+
+				currently supported types:
+				skill, ${SKILLS.join(', ')}
+				${COSMETIC_SKILLS.join(', ')}
+				slayer, revenant, tarantula, sven, ${SLAYERS.join(', ')}
+				dungeon, ${[ ...DUNGEON_TYPES, ...DUNGEON_CLASSES ].join(', ')}
+				weight
+			`,
+			cooldown: 1,
 		});
 	}
 
@@ -25,22 +38,67 @@ module.exports = class TracklistCommand extends Command {
 	 * @param {string[]} rawArgs arguments and flags
 	 */
 	async run(message, args, flags, rawArgs) {
-		const type = 'Weight';
-		const player = message.author.player;
+		/**
+		 * @type {import('../../structures/database/models/Player')}
+		 */
+		const player = message.mentions.users.size
+			? message.mentions.users.first().player
+			: (() => {
+				for (const arg of args) {
+					const result = this.client.players.getByIGN(arg);
+					if (result) return result;
+				}
+			})() ?? message.author.player;
+
+		if (!player) {
+			return message.reply(oneLine`${message.mentions.users.size
+				? `\`${message.guild
+					? message.mentions.members.first().displayName
+					: message.mentions.users.first().username}\`
+					 is`
+				: args.length
+					? `\`${args[0]}\` is`
+					: 'you are'
+			} not in the player db.`);
+		}
+
+		let type = 'weight';
+
+		for (const arg of args) {
+			const result = autocorrect(arg, [ 'skill', 'slayer', 'revenant', 'tarantula', 'sven', 'dungeon', 'gxp', 'weight', ...XP_TYPES ]);
+
+			if (result.similarity < this.client.config.get('AUTOCORRECT_THRESHOLD')) continue;
+
+			type = result.value;
+
+			switch (type) {
+				case 'revenant':
+					type = 'zombie';
+					break;
+				case 'tarantula':
+					type = 'spider';
+					break;
+				case 'sven':
+					type = 'wolf';
+					break;
+				case 'dungeon':
+					type = 'catacombs';
+					break;
+				case 'gxp':
+					type = 'guild';
+					break;
+			}
+		}
+
 		const days = 30;
 
-		const canvas = new ChartJSNodeCanvas({
-			width: 800,
-			height: 400,
-		});
+		let datasets;
 
-		const weightHistory = [ ...Array(days).keys() ].map(x => player.getWeightHistory(x));
+		switch (type) {
+			case 'weight': {
+				const weightHistory = [ ...Array(days).keys() ].map(x => player.getWeightHistory(x));
 
-		const image = await canvas.renderToBuffer({
-			type: 'line',
-			data: {
-				labels: [ ...Array(days).keys() ].map(x => days - 1 - x),
-				datasets: [{
+				datasets = [{
 					label: 'Weight',
 					backgroundColor: 'rgba(0, 0, 255, 0.25)',
 					borderColor: 'rgb(0, 0, 128)',
@@ -55,7 +113,69 @@ module.exports = class TracklistCommand extends Command {
 					backgroundColor: 'rgba(255, 0, 0, 0.25)',
 					borderColor: 'rgb(128, 0, 0)',
 					data: weightHistory.map(x => x.totalWeight),
-				}],
+				}];
+				break;
+			}
+
+			case 'skill': {
+				const skillAverageHistory = [ ...Array(days).keys() ].map(x => player.getSkillAverageHistory(x));
+
+				datasets = [{
+					label: 'Skill Average',
+					backgroundColor: 'rgba(0, 0, 255, 0.25)',
+					borderColor: 'rgb(0, 0, 128)',
+					data: skillAverageHistory.map(x => x.skillAverage),
+				}, {
+					label: 'True Average',
+					backgroundColor: 'rgba(0, 255, 0, 0.25)',
+					borderColor: 'rgb(0, 128, 0)',
+					data: skillAverageHistory.map(x => x.trueAverage),
+				}];
+				break;
+			}
+
+			case 'slayer': {
+				datasets = [{
+					label: 'Slayer XP',
+					backgroundColor: 'rgba(0, 0, 255, 0.25)',
+					borderColor: 'rgb(0, 0, 128)',
+					data: [ ...Array(days).keys() ].map(x => player.getSlayerTotalHistory(x)),
+				}];
+				break;
+			}
+
+			case 'zombie':
+			case 'spider':
+			case 'wolf': {
+				datasets = [{
+					label: upperCaseFirstChar(type),
+					backgroundColor: 'rgba(0, 0, 255, 0.25)',
+					borderColor: 'rgb(0, 0, 128)',
+					data: [ ...Array(days).keys() ].map(x => player[`${type}XpHistory`][x]),
+				}];
+				break;
+			}
+
+			default: {
+				datasets = [{
+					label: upperCaseFirstChar(type),
+					backgroundColor: 'rgba(0, 0, 255, 0.25)',
+					borderColor: 'rgb(0, 0, 128)',
+					data: [ ...Array(days).keys() ].map(x => player.getSkillLevelHistory(type, x).nonFlooredLevel),
+				}];
+			}
+		}
+
+		const canvas = new ChartJSNodeCanvas({
+			width: 800,
+			height: 400,
+		});
+
+		const image = await canvas.renderToBuffer({
+			type: 'line',
+			data: {
+				labels: [ ...Array(days).keys() ].map(x => days - 1 - x),
+				datasets,
 			},
 			options: {
 				scales: {
@@ -70,7 +190,8 @@ module.exports = class TracklistCommand extends Command {
 
 		message.reply(new MessageEmbed()
 			.setColor(this.client.config.get('EMBED_BLUE'))
-			.setTitle(`${type}`)
+			.setAuthor(`${player.ign}${player.mainProfileName ? ` (${player.mainProfileName})` : ''}`, player.image, player.url)
+			.setTitle(`${upperCaseFirstChar(datasets[0].label)} history (${days} days)`)
 			.attachFiles(new MessageAttachment(image))
 			.setImage('attachment://file.jpg')
 			.setTimestamp(),
