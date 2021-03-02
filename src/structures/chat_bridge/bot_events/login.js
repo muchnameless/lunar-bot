710b2fbc752aa69c6fdca7161518f436b10407ae
muchnameless <muchnameless@gmail.com>
2021-03-02 19:32:29 +0100

    fix(MinecraftBot#login): send forbidden character with /ac

--- src/structures/chat_bridge/bot_events/login.js
+++ src/structures/chat_bridge/bot_events/login.js
@@ -13,5 +13,5 @@ module.exports = async chatBridge => {
 	chatBridge.bot.write('settings', defaultSettings);
 
 	// send bot to limbo (forbidden character in chat)
-	chatBridge.queueForMinecraftChat('§');
+	chatBridge.queueForMinecraftChat('/ac §');
 };
