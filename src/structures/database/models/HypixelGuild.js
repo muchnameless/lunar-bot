51dde33fbe2d4cedb145b1a2ab890be9f991c549
muchnameless <muchnameless@gmail.com>
2021-03-09 17:29:24 +0100

    revert(HypixelGuild)

--- src/structures/database/models/HypixelGuild.js
+++ src/structures/database/models/HypixelGuild.js
@@ -585,10 +585,14 @@ module.exports = class HypixelGuild extends Model {
 	 */
 	async handleChatBridgeMessage(message) {
 		try {
-			// message is from chatBridge webhook or chatbridge disabled, message is from bot
-			if (message.webhookID === chatBridge.webhook.id || !this.client.config.getBoolean('CHATBRIDGE_ENABLED') || message.author.id === this.client.user.id) return;
+			// chatbridge disabled, message is from bot or no message.content to chat
+			if (!this.client.config.getBoolean('CHATBRIDGE_ENABLED') || message.author.id === this.client.user.id) return;
 
 			const { chatBridge } = this;
+
+			// message is from chatBridge webhook
+			if (message.webhookID === chatBridge.webhook.id) return;
+
 			/**
 			 * @type {import('./Player')}
 			 */
