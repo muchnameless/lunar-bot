163da6ad0c0d204dea8c5d5af65a8092235d041e
muchnameless <muchnameless@gmail.com>
2021-03-12 18:10:07 +0100

    feat(stringValidators)

---
+++ src/functions/stringValidators.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * checks if the string is a number
+ * @param {string} string
+ */
+module.exports.validateNumber = string => /^\d+$/.test(string);
+
+/**
+ * checks if the string can be a discord tag
+ * @param {string} string
+ */
+module.exports.validateDiscordTag = string => /^.{2,32}#\d{4}$/.test(string);
+
+/**
+ * checks if the string can be a minecraft IGN
+ * @param {string} string
+ */
+module.exports.validateMinecraftIGN = string => /^\w{3,16}$/.test(string);
+
+/**
+ * checks if the string can be a minecraft IGN
+ * @param {string} string
+ */
+module.exports.validateMinecraftUUID = string => /^[0-9a-f]{8}-?(?:[0-9a-f]{4}-?){3}[0-9a-f]{12}$/i.test(string);
