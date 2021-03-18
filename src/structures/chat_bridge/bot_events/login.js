b44068ecefc164e33e79fcd3b93c0ac9883316d4
muchnameless <muchnameless@gmail.com>
2021-03-19 00:37:59 +0100

    refactor(MinecraftBot): clean uuid on login

--- src/structures/chat_bridge/bot_events/login.js
+++ src/structures/chat_bridge/bot_events/login.js
@@ -10,6 +10,10 @@ const logger = require('../../../functions/logger');
 module.exports = async (chatBridge) => {
 	logger.debug(`[CHATBRIDGE]: ${chatBridge.bot.username}: logged in, sending to limbo`);
 
+	// remove '-' from uuid
+	chatBridge.bot.uuid = chatBridge.bot.uuid.replace(/-/g, '');
+
+	// send settings to server
 	chatBridge.bot.write('settings', defaultSettings);
 
 	// send bot to limbo (forbidden character in chat)
