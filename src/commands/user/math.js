d0c39b2e3849d519ade045463f4560aa0ecb8846
muchnameless <muchnameless@gmail.com>
2021-03-23 23:00:10 +0100

    feat(math)

---
+++ src/commands/user/math.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Command = require('../../structures/commands/Command');
+// const logger = require('../../functions/logger');
+
+
+module.exports = class MathCommand extends Command {
+	constructor(data) {
+		super(data, {
+			aliases: [ 'calc' ],
+			description: '',
+			args: false,
+			usage: '',
+			cooldown: 0,
+		});
+	}
+
+	/**
+	 * execute the command
+	 * @param {import('../../structures/extensions/Message')} message message that triggered the command
+	 * @param {string[]} args command arguments
+	 * @param {string[]} flags command flags
+	 * @param {string[]} rawArgs arguments and flags
+	 */
+	async run(message, args, flags, rawArgs) { // eslint-disable-line no-unused-vars
+		const INPUT = rawArgs.join('')
+			.replace(/\^/g, '**')
+			.replace(/[^\d+\-*/%.()]/g, '');
+		const OUTPUT = eval(INPUT);
+
+		message.reply(`${INPUT} = ${OUTPUT}`);
+	}
+};
