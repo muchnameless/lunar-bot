09567f99b3fd823fccc9edd45184d9a5a1fcb34d
muchnameless <muchnameless@gmail.com>
2021-03-23 02:29:33 +0100

    refactor(weight)

--- src/commands/user/weight.js
+++ src/commands/user/weight.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const { upperCaseFirstChar } = require('../../functions/util');
-const senither = require('../../api/senither');
-const mojang = require('../../api/mojang');
-const MojangAPIError = require('../../structures/errors/MojangAPIError');
+const weight = require('../../functions/weight');
 const Command = require('../../structures/commands/Command');
-const logger = require('../../functions/logger');
+// const logger = require('../../functions/logger');
 
 
 module.exports = class WeightCommand extends Command {
@@ -20,14 +17,6 @@ module.exports = class WeightCommand extends Command {
 	}
 
 	/**
-	 * rounds and toLocaleStrings a number
-	 * @param {number} number
-	 */
-	formatNumber(number) {
-		return this.client.formatDecimalNumber(Math.floor(number * 100) / 100);
-	}
-
-	/**
 	 * execute the command
 	 * @param {import('../../structures/extensions/Message')} message message that triggered the command
 	 * @param {string[]} args command arguments
@@ -35,25 +24,6 @@ module.exports = class WeightCommand extends Command {
 	 * @param {string[]} rawArgs arguments and flags
 	 */
 	async run(message, args, flags, rawArgs) { // eslint-disable-line no-unused-vars
-		try {
-			const uuid = args.length
-				? await mojang.getUUID(args[0])
-				: message.author.player?.minecraftUUID ?? await mojang.getUUID(message.author.ign);
-			const data = await senither.profiles.uuid(uuid, args.length < 2 ? 'weight' : null);
-			const { username, name, weight, weight_overflow: overflow } = args.length < 2 ? data : data.find(({ name: profileName }) => profileName.toLowerCase() === args[1].toLowerCase()) ?? (() => { throw new Error(`unknown profile name '${upperCaseFirstChar(args[1].toLowerCase())}'`); })();
-
-			return message.reply(`${username} (${name}): ${this.formatNumber(weight + overflow)} [${this.formatNumber(weight)} + ${this.formatNumber(overflow)}]`);
-		} catch (error) {
-			logger.error(`[WEIGHT]: ${error instanceof MojangAPIError
-					? `${error}`
-					: error.message
-			}`);
-
-			return message.reply(
-				error instanceof MojangAPIError
-					? `${error}`
-					: error.message,
-			);
-		}
+		return weight(message, args);
 	}
 };
---
+++ src/functions/weight.js
@@ -0,0 +1,45 @@
+'use strict';
+const { upperCaseFirstChar } = require('../functions/util');
+const { X_EMOJI } = require('../constants/emojiCharacters');
+const mojang = require('../api/mojang');
+const senither = require('../api/senither');
+const MojangAPIError = require('../structures/errors/MojangAPIError');
+const logger = require('../functions/logger');
+
+
+/**
+ * weight command
+ * @param {import('../structures/extensions/Message')|import('../structures/chat_bridge/HypixelMessage')} message message that triggered the command
+ * @param {string[]} args command arguments
+ */
+module.exports = async (message, args) => {
+	try {
+		const uuid = args.length
+			? await mojang.getUUID(args[0])
+			: message.author.player?.minecraftUUID ?? await mojang.getUUID(message.author.ign);
+		const data = await senither.profiles.uuid(uuid, args.length < 2 ? 'weight' : null);
+		const { username, name, weight, weight_overflow: overflow, skills: { apiEnabled } } = args.length < 2
+			? data
+			: (data.find(({ name: profileName }) => profileName.toLowerCase() === args[1].toLowerCase()) ?? (() => { throw new Error(`unknown profile name '${upperCaseFirstChar(args[1].toLowerCase())}'`); })());
+
+		/**
+		 * rounds and toLocaleStrings a number
+		 * @param {number} number
+		 * @returns {string}
+		 */
+		const formatNumber = number => message.client.formatDecimalNumber(Math.floor(number * 100) / 100);
+
+		return message.reply(`${username} (${name}): ${formatNumber(weight + overflow)} [${formatNumber(weight)} + ${formatNumber(overflow)}]${apiEnabled ? '' : ` (${X_EMOJI} API disabled)`}`);
+	} catch (error) {
+		logger.error(`[WEIGHT]: ${error instanceof MojangAPIError
+				? `${error}`
+				: error.message
+		}`);
+
+		return message.reply(
+			error instanceof MojangAPIError
+				? `${error}`
+				: error.message,
+		);
+	}
+};
