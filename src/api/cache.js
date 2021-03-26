cb98b0475271ceea2a9a49245bef2d2ef07cf7b6
muchnameless <muchnameless@gmail.com>
2021-03-26 06:05:02 +0100

    feat(cache): add redis cache

--- src/api/cache.js
+++ src/api/cache.js
@@ -1,8 +1,26 @@
 'use strict';
 
 const cacheManager = require('cache-manager');
+const redisStore = require('cache-manager-redis');
+const logger = require('../functions/logger');
 
 
-const apiCache = cacheManager.caching({ store: 'memory' });
+const memoryCache = cacheManager.caching({
+	store: 'memory',
+	max: 100,
+	ttl: 60,
+});
+
+const redisCache = cacheManager.caching({
+	store: redisStore,
+	host: 'localhost', // default value
+	port: 6379, // default value
+	db: 0,
+	ttl: 600,
+});
+
+redisCache.store.events.on('redisError', error => logger.error(error));
+
+const apiCache = cacheManager.multiCaching([ memoryCache, redisCache ]);
 
 module.exports = apiCache;
