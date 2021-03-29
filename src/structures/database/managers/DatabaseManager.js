aad9e578d04510a44ecfe72eab82b88453ce70bf
muchnameless <muchnameless@gmail.com>
2021-03-29 08:39:48 +0200

    refactor(DatabaseManager#update): separate ign and xp updates

--- src/structures/database/managers/DatabaseManager.js
+++ src/structures/database/managers/DatabaseManager.js
@@ -275,8 +275,10 @@ class DatabaseManager {
 		// update tax db
 		const availableAuctionsLog = config.getBoolean('TAX_TRACKING_ENABLED') ? await this._updateTaxDatabase() : null;
 
+		players.updateIGN();
+
 		// update Xp
-		if (config.getBoolean('XP_TRACKING_ENABLED')) players.update();
+		if (config.getBoolean('XP_TRACKING_ENABLED')) players.updateXp();
 
 		// update taxMessage
 		const taxChannel = this.client.channels.cache.get(config.get('TAX_CHANNEL_ID'));
