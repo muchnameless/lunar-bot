c6fdee88fbcbf204d1134f393098249cb31f3c6a
muchnameless <muchnameless@gmail.com>
2021-04-05 15:41:23 +0200

    fix(math): unary - in brackets

--- src/commands/general/maths.js
+++ src/commands/general/maths.js
@@ -68,7 +68,7 @@ class Parser {
 
 const lexer = new Lexer()
 	.addRule(/[\s,]/, () => void 0) // ignore whitespaces and ','
-	.addRule(/(\d+(?:\.\d+)?|\.\d+)|[(+\-*/)^!]/, lexeme => lexeme)
+	.addRule(/(?:(?<=\D\s*)-)?(\d+(?:\.\d+)?|\.\d+)|[(+\-*/)^!]/, lexeme => lexeme)
 	.addRule(/sin(?:e|us)?/i, () => 'sin') // functions
 	.addRule(/cos(?:ine|inus)?/i, () => 'cos')
 	.addRule(/tan(?:gen[st])?/i, () => 'tan')
