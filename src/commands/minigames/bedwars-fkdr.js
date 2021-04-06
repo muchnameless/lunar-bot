bcd701c6dc3cae47b05e69cd339a6c5a8d77602d
muchnameless <muchnameless@gmail.com>
2021-04-06 23:18:52 +0200

    refactor(commands): minigames

--- src/commands/minigames/bedwars-fkdr.js
+++ src/commands/minigames/bedwars-fkdr.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const BwStatsCommand = require('./bwstats');
+const BwStatsCommand = require('./bedwars-stats');
 // const logger = require('../../functions/logger');
 
 
 module.exports = class FkdrCommand extends BwStatsCommand {
 	constructor(data) {
 		super(data, {
-			aliases: [],
+			aliases: [ 'fkdr' ],
 			description: 'shows a player\'s BedWars fkdr',
 			args: false,
 			usage: '<`IGN`>',
@@ -16,17 +16,8 @@ module.exports = class FkdrCommand extends BwStatsCommand {
 	}
 
 	/**
-	 * @param {number} kills
-	 * @param {number} deaths
-	 */
-	calculateKD(kills, deaths) {
-		if (kills == null || deaths == null) return null;
-		return this.client.formatDecimalNumber(Math.floor((kills / deaths) * 100) / 100);
-	}
-
-	/**
 	 * @param {string} ign
-	 * @param {import('@zikeji/hypixel').Components.Schemas.PlayerStatsBedwars} data
+	 * @param {import('@zikeji/hypixel').Components.Schemas.Player} data
 	 */
 	generateReply(ign, data) {
 		const kds = [
@@ -36,15 +27,15 @@ module.exports = class FkdrCommand extends BwStatsCommand {
 			{ name: '3s', key: 'four_three_' },
 			{ name: '4s', key: 'four_four_' },
 		].flatMap(({ name, key }) => {
-			const kd = this.calculateKD(data[`${key}final_kills_bedwars`], data[`${key}final_deaths_bedwars`]);
+			const kd = this.calculateKD(data.stats.Bedwars[`${key}final_kills_bedwars`], data.stats.Bedwars[`${key}final_deaths_bedwars`]);
 
 			return kd !== null
 				? ({ name, kd })
 				: [];
 		});
 
-		if (!kds.length) return `\`${ign}\` has no bed wars stats`;
+		if (!kds.length) return `\`${ign}\` has no BedWars stats`;
 
-		return `${ign}: ${kds.map(({ name, kd }) => `${name}: ${kd}`).join(', ')}`;
+		return `${ign}: BedWars: ${kds.map(({ name, kd }) => `${name}: ${kd}`).join(', ')}`;
 	}
 };
