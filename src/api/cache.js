91f08ced1930b7de7ee0e6202cbae0458d0c8094
muchnameless <muchnameless@gmail.com>
2021-04-11 14:11:03 +0200

    refactor(cache): consistency

--- src/api/cache.js
+++ src/api/cache.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const Keyv = require('keyv');
-
 const logger = require('../functions/logger');
+
+
 const keyv = new Keyv(process.env.REDIS_URI, {
 	namespace: process.env.NAMESPACE,
 	ttl: 10 * 60_000,
