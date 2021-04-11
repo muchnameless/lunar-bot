605736baace2262e2c3b807a08d2665ad17daa5f
muchnameless <muchnameless@gmail.com>
2021-04-11 02:11:06 +0200

    feat(maths): allow 'x' instead of '*'

--- src/commands/general/maths.js
+++ src/commands/general/maths.js
@@ -254,7 +254,8 @@ module.exports = class MathCommand extends Command {
 		const INPUT = rawArgs.join(' ')
 			.replace(/\*\*/g, '^')
 			.replace(/:/g, '/') // 5:3 -> 5/3
-			.replace(/(?<=\d\s*)(?=[a-z(])/gi, '*'); // add implicit '*' between numbers before letters and '('
+			.replace(/(?<=\d\s*)(?=[a-z(])/gi, '*') // add implicit '*' between numbers before letters and '('
+			.replace(/(?<=\*)x/gi, ''); // 5x3 -> 5*3
 
 		let parsed;
 
