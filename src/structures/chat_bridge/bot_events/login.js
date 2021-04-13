b2f4ebec311e97cb49f5eaf7d5d8f35325d12b24
muchnameless <muchnameless@gmail.com>
2021-04-13 10:56:13 +0200

    feat(MinecraftBot#login): retry sending to limbo and better logging

--- src/structures/chat_bridge/bot_events/login.js
+++ src/structures/chat_bridge/bot_events/login.js
@@ -8,7 +8,7 @@ const logger = require('../../../functions/logger');
  * @param {import('../ChatBridge')} chatBridge
  */
 module.exports = async (chatBridge) => {
-	logger.debug(`[CHATBRIDGE]: ${chatBridge.bot.ign}: logged in, sending to limbo`);
+	logger.debug(`[CHATBRIDGE]: ${chatBridge.bot.ign}: logged in`);
 
 	// remove '-' from uuid
 	chatBridge.bot.uuid = chatBridge.bot.uuid.replace(/-/g, '');
@@ -17,5 +17,20 @@ module.exports = async (chatBridge) => {
 	chatBridge.bot.write('settings', defaultSettings);
 
 	// send bot to limbo (forbidden character in chat)
-	chatBridge.sendToMinecraftChat('/ac §');
+	let counter = 5;
+
+	do {
+		try {
+			await chatBridge.command({
+				command: 'ac §',
+				responseRegex: /^A kick occurred in your connection, so you have been routed to limbo!$|^Illegal characters in chat$|^You were spawned in Limbo\.$|^\/limbo for more information\.$/,
+				rejectOnTimeout: true,
+			});
+
+			logger.debug(`[CHATBRIDGE]: ${chatBridge.bot.ign}: sent to limbo`);
+			break;
+		} catch (error) {
+			logger.error(`[CHATBRIDGE]: ${chatBridge.bot.ign}: error while sending to limbo: ${error}`);
+		}
+	} while (--counter);
 };
