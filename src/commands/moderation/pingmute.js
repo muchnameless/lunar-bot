9cafb4976908c4cf222a2cabb2ea34366a92f45d
muchnameless <muchnameless@gmail.com>
2021-04-22 15:33:58 +0200

    feat(commands): ping(un)mute

---
+++ src/commands/moderation/pingmute.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const { oneLine } = require('common-tags');
+const Command = require('../../structures/commands/Command');
+// const logger = require('../../functions/logger');
+
+
+module.exports = class PingMuteCommand extends Command {
+	constructor(data) {
+		super(data, {
+			aliases: [],
+			description: 'prevent a guild member from @mentioning via the chat bridge',
+			args: 1,
+			usage: '[`ign`|`discord id`|`@mention`]',
+			cooldown: 0,
+		});
+	}
+
+	/**
+	 * execute the command
+	 * @param {import('../../structures/extensions/Message')} message message that triggered the command
+	 * @param {string[]} args command arguments
+	 * @param {string[]} flags command flags
+	 * @param {string[]} rawArgs arguments and flags
+	 */
+	async run(message, args, flags, rawArgs) { // eslint-disable-line no-unused-vars
+		/** @type {import('../../structures/database/models/Player')} */
+		const player = message.mentions.users.size
+			? message.mentions.users.first().player
+			: (this.client.players.getByID(args[0]) ?? this.client.players.getByIGN(args[0]));
+
+		if (!player) {
+			return message.reply(oneLine`${message.mentions.users.size
+				? `\`${message.guild
+					? message.mentions.members.first().displayName
+					: message.mentions.users.first().username}\`
+					is`
+				: `\`${args[0]}\``
+			} not in the player db.`);
+		}
+
+		player.hasDiscordPingPermission = false;
+		await player.save();
+
+		return message.reply(`\`${player.ign}\` can no longer ping members via the chat bridge`);
+	}
+};
