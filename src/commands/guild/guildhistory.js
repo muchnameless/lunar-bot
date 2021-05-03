7c700a97cb1c60386528b77938987f2470650876
muchnameless <muchnameless@gmail.com>
2021-05-03 21:00:36 +0200

    feat(commands): guildlog, guildhistory

---
+++ src/commands/guild/guildhistory.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const { MessageEmbed } = require('discord.js');
+const Command = require('../../structures/commands/Command');
+// const logger = require('../../functions/logger');
+
+
+module.exports = class GuildListCommand extends Command {
+	constructor(data, options) {
+		super(data, options ?? {
+			aliases: [ 'ghistory' ],
+			description: 'guild history',
+			args: false,
+			usage: '',
+			cooldown: 1,
+		});
+	}
+
+	/**
+	 * execute the command
+	 * @param {import('../../structures/extensions/Message')} message message that triggered the command
+	 * @param {string[]} args command arguments
+	 * @param {string[]} flags command flags
+	 * @param {string[]} rawArgs arguments and flags
+	 */
+	async run(message, args, flags, rawArgs) { // eslint-disable-line no-unused-vars
+		/**
+		 * @type {import('../../structures/database/models/HypixelGuild')}
+		 */
+		const hypixelGuild = this.client.hypixelGuilds.getFromArray(rawArgs) ?? message.author.player?.guild;
+
+		if (!hypixelGuild) return message.reply('unable to find your guild.');
+
+		const command = 'g history';
+
+		const data = await hypixelGuild.chatBridge.minecraft.command({
+			command,
+		});
+
+		return message.reply(
+			new MessageEmbed()
+				.setColor(this.config.get('EMBED_BLUE'))
+				.setTitle(`/${command}`)
+				.setDescription(`\`\`\`\n${data}\`\`\``)
+				.setTimestamp(),
+		);
+	}
+};
