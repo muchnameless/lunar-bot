4d86c74bcc0fb4622de985b0ade758bb189ad0f6
muchnameless <muchnameless@gmail.com>
2021-05-29 04:07:00 +0200

    refactor(MinecraftChatManager): exponential loginDelay

--- src/structures/chat_bridge/managers/MinecraftChatManager.js
+++ src/structures/chat_bridge/managers/MinecraftChatManager.js
@@ -322,7 +322,7 @@ module.exports = class MinecraftChatManager extends ChatManager {
 		// reconnect the bot if it hasn't successfully spawned in 60 seconds
 		this.abortLoginTimeout = setTimeout(() => {
 			logger.warn('[CHATBRIDGE ABORT TIMER]: login abort triggered');
-			this.reconnect(0);
+			this.reconnect();
 		}, Math.min(++this.loginAttempts * 60_000, 300_000));
 
 		await this._createBot();
@@ -336,7 +336,7 @@ module.exports = class MinecraftChatManager extends ChatManager {
 	 * reconnects the bot
 	 * @param {?number} loginDelay delay in ms
 	 */
-	reconnect(loginDelay = Math.min(this.loginAttempts * 5_000, 300_000)) {
+	reconnect(loginDelay = Math.min(Math.exp(this.loginAttempts), 300_000)) {
 		// prevent multiple reconnections
 		if (this._isReconnecting) return this.chatBridge;
 		this._isReconnecting = true;
