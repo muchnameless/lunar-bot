872b2273b8e652e99a09adc577f707d74aaf3f4e
muchnameless <muchnameless@gmail.com>
2021-05-31 04:26:43 +0200

    types(*): add d.ts

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+declare module 'discord.js' {
+	interface Message {
+		client: import('./src/structures/LunarClient');
+		// author: import('./src/structures/extensions/User');
+	}
+
+	interface CommandInteraction {
+		client: import('./src/structures/LunarClient');
+	}
+
+	interface User {
+		client: import('./src/structures/LunarClient');
+		player: import('./src/structures/database/models/Player');
+	}
+
+	interface Channel {
+		botPermissions: import('discord.js').Permissions
+	}
+
+	interface Guild {
+		client: import('./src/structures/LunarClient');
+	}
+
+	interface GuildMember {
+		client: import('./src/structures/LunarClient');
+		player: import('./src/structures/database/models/Player');
+		// user: import('./src/structures/extensions/User');
+	}
+
+	interface Base {
+		// client: import('./src/structures/LunarClient');
+	}
+}
