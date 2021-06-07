fa91893ebb0f6a13ba2f1fa2e55c588ecfb93561
muchnameless <muchnameless@gmail.com>
2021-06-07 02:45:52 +0200

    feat(slashcommand_blueprint)

---
+++ src/structures/commands/slashcommand_blueprint.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const SlashCommand = require('../../structures/commands/SlashCommand');
+// const logger = require('../functions/logger');
+
+
+module.exports = class MyCommand extends SlashCommand {
+	/**
+	 * @param {import('../../structures/commands/SlashCommand').CommandData} commandData
+	 */
+	constructor(data, commandData) {
+		super(data, commandData ?? {
+			aliases: [],
+			description: '',
+			options: [],
+			defaultPermission: true,
+			cooldown: 0,
+		});
+	}
+
+	/**
+	 * execute the command
+	 * @param {import('../../structures/extensions/CommandInteraction')} interaction
+	 */
+	async run(interaction) { // eslint-disable-line no-unused-vars
+		// do stuff
+	}
+};
