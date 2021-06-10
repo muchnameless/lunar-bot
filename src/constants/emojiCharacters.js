486759c31c0b27cb141157618286bc3af59e9585
muchnameless <muchnameless@gmail.com>
2021-06-10 15:30:36 +0200

    refactor(constants.emojiCharacters): debloat

--- src/constants/emojiCharacters.js
+++ src/constants/emojiCharacters.js
@@ -7,22 +7,14 @@ module.exports = {
 	Y_EMOJI_ALT: '✔️',
 
 	// arrows
-	SORT_ASCENDING_EMOJI: '⬆️',
-	SORT_DESCENDING_EMOJI: '⬇️',
 	LEFT_EMOJI: '◀️',
 	RIGHT_EMOJI: '▶️',
 	RELOAD_EMOJI: '🔄',
 	DOUBLE_RIGHT_EMOJI: '⏭️',
 	DOUBLE_LEFT_EMOJI: '⏮️',
-	LEFT_EMOJI_ALT: '⬅️',
-	RIGHT_EMOJI_ALT: '➡️',
-	DOUBLE_LEFT_EMOJI_ALT: '⏪',
-	DOUBLE_RIGHT_EMOJI_ALT: '⏩',
 
 	// misc
-	FLUSHED: '😳',
 	CLOWN: '🤡',
-	LOCK: '🔒',
 	MUTED: '🔇',
 	STOP: '🛑',
 	NO_BELL: '🔕',
