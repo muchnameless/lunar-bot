7db2538f2dab445122f1097d00e7fd029b0d22d6
muchnameless <muchnameless@gmail.com>
2021-06-12 01:07:26 +0200

    refactor(events.ready): remove temp /command registerer

--- src/events/ready.js
+++ src/events/ready.js
@@ -11,11 +11,6 @@ const logger = require('../functions/logger');
 module.exports = async (client) => {
 	logger.debug(`[READY]: logged in as ${client.user.tag}`);
 
-	// client.commands.init(client.lgGuild.commands).catch((err) => {
-	// 	logger.error(err);
-	// 	logger.error(err.requestData.json.flatMap(x => [ x.name, x.options ]));
-	// })
-
 	await client.logHandler.init();
 
 	// Fetch all members for initially available guilds
