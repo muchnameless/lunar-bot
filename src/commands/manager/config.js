37d449de41c47eb77fa6457a75dbfce3c613e7a4
muchnameless <muchnameless@gmail.com>
2021-06-13 06:16:54 +0200

    refactor(commands.config): sub commands

--- src/commands/manager/config.js
+++ src/commands/manager/config.js
@@ -11,20 +11,40 @@ module.exports = class ConfigCommand extends SlashCommand {
 			aliases: [],
 			description: 'show and edit the bot\'s config',
 			options: [{
-				name: 'key',
-				type: Constants.ApplicationCommandOptionTypes.STRING,
-				description: 'new / existing config key',
-				required: false,
-			}, {
-				name: 'value',
-				type: Constants.ApplicationCommandOptionTypes.STRING,
-				description: 'new config value',
-				required: false,
+				name: 'edit',
+				type: Constants.ApplicationCommandOptionTypes.SUB_COMMAND,
+				description: 'edit the config key with the provided value',
+				options: [{
+					name: 'key',
+					type: Constants.ApplicationCommandOptionTypes.STRING,
+					description: 'new / existing config key',
+					required: true,
+				}, {
+					name: 'value',
+					type: Constants.ApplicationCommandOptionTypes.STRING,
+					description: 'new config value',
+					required: true,
+				}],
 			}, {
 				name: 'delete',
-				type: Constants.ApplicationCommandOptionTypes.BOOLEAN,
-				description: 'delete the selected key',
-				required: false,
+				type: Constants.ApplicationCommandOptionTypes.SUB_COMMAND,
+				description: 'delete the config key',
+				options: [{
+					name: 'key',
+					type: Constants.ApplicationCommandOptionTypes.STRING,
+					description: 'new / existing config key',
+					required: true,
+				}],
+			}, {
+				name: 'search',
+				type: Constants.ApplicationCommandOptionTypes.SUB_COMMAND,
+				description: 'searches the config keys and values',
+				options: [{
+					name: 'query',
+					type: Constants.ApplicationCommandOptionTypes.STRING,
+					description: 'search query',
+					required: false,
+				}],
 			}],
 			defaultPermission: true,
 			cooldown: 0,
@@ -35,25 +55,24 @@ module.exports = class ConfigCommand extends SlashCommand {
 	 * execute the command
 	 * @param {import('../../structures/extensions/CommandInteraction')} interaction
 	 */
-	async run(interaction) { // eslint-disable-line no-unused-vars
-		// list all config entries
-		if (!interaction.options.size) {
-			return interaction.reply({
-				content: this.config.cache
-					.sorted(({ key: keyA }, { key: keyB }) => keyA.localeCompare(keyB))
-					.map(({ key, value }) => `${key}: ${value}`)
-					.join('\n'),
-				code: 'apache',
-				split: { char: '\n' },
-			});
-		}
+	async run(interaction) {
+		// destructure sub command
+		const { name, options } = interaction.options.first();
 
-		const KEY = interaction.options.get('key')?.value.toUpperCase() ?? (() => { throw 'specifiy a config key to edit'; })();
+		switch (name) {
+			case 'edit': {
+				const KEY = options.get('key').value.toUpperCase();
+				const OLD_VALUE = this.config.get(KEY);
+				const { key, value } = await this.config.set(KEY, options.get('value').value);
 
-		// search the config
-		if (!interaction.options.has('value')) {
-			// delete an existing entry
-			if (interaction.options.get('delete')?.value) {
+				return interaction.reply({
+					content: `${key}: ${OLD_VALUE !== null ? `'${OLD_VALUE}' -> ` : ''}'${value}'`,
+					code: 'apache',
+				});
+			}
+
+			case 'delete': {
+				const KEY = options.get('key').value.toUpperCase();
 				const VALUE = this.config.get(KEY);
 
 				if (VALUE === null) return interaction.reply(`\`${KEY}\` is not in the config`);
@@ -62,27 +81,34 @@ module.exports = class ConfigCommand extends SlashCommand {
 				return interaction.reply(`removed \`${KEY}\`: \`${VALUE}\``);
 			}
 
-			const queryRegex = new RegExp(KEY, 'i');
+			case 'search': {
+				const query = options?.get('query')?.value;
 
-			return interaction.reply({
-				content: this.config.cache
-					.filter(({ key, value }) => queryRegex.test(key) || queryRegex.test(value))
-					.sorted(({ key: keyA }, { key: keyB }) => keyA.localeCompare(keyB))
-					.map(({ key, value }) => `${key}: ${value}`)
-					.join('\n')
-					|| `no config entries for '${KEY}' found`,
-				code: 'apache',
-				split: { char: '\n' },
-			});
+				if (!query) return interaction.reply({
+					content: this.config.cache
+						.sorted(({ key: keyA }, { key: keyB }) => keyA.localeCompare(keyB))
+						.map(({ key, value }) => `${key}: ${value}`)
+						.join('\n'),
+					code: 'apache',
+					split: { char: '\n' },
+				});
+
+				const queryRegex = new RegExp(query, 'i');
+
+				return interaction.reply({
+					content: this.config.cache
+						.filter(({ key, value }) => queryRegex.test(key) || queryRegex.test(value))
+						.sorted(({ key: keyA }, { key: keyB }) => keyA.localeCompare(keyB))
+						.map(({ key, value }) => `${key}: ${value}`)
+						.join('\n')
+						|| `no config entries for '${query}' found`,
+					code: 'apache',
+					split: { char: '\n' },
+				});
+			}
+
+			default:
+				throw new Error('unknown command');
 		}
-
-		// set a config entry
-		const OLD_VALUE = this.config.get(KEY);
-		const { key, value } = await this.config.set(KEY, interaction.options.get('value').value);
-
-		return interaction.reply({
-			content: `${key}: ${OLD_VALUE !== null ? `'${OLD_VALUE}' -> ` : ''}'${value}'`,
-			code: 'apache',
-		});
 	}
 };
