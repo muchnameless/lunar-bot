ad40f1f3d101b9da9c092fdb0a54ef9d92645dfc
muchnameless <muchnameless@gmail.com>
2021-06-14 13:48:50 +0200

    fix(interactions#useEphemeral): ephem option for sub commands

--- src/structures/extensions/ButtonInteraction.js
+++ src/structures/extensions/ButtonInteraction.js
@@ -25,6 +25,7 @@ class LunarButtonInteraction extends ButtonInteraction {
 		 * wether to use ephemeral replies and deferring
 		 */
 		this.useEphemeral = this.options.get('ephemeral')?.value
+			?? this.options.first()?.options?.get('ephemeral')?.value
 			?? (channel !== null && channel.type !== 'dm'
 				? !(channel.name.includes('command') || channel.isTicket) // guild channel
 				: false); // DM channel
