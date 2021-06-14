78f328fcbba4b5e0f1f434e6c66574372b138d51
muchnameless <muchnameless@gmail.com>
2021-06-15 00:49:43 +0200

    refactor(handleCommandInteraction): improved code flow, logging

--- src/functions/handleCommandInteraction.js
+++ src/functions/handleCommandInteraction.js
@@ -9,11 +9,11 @@ const logger = require('./logger');
  * @param {import('../structures/extensions/CommandInteraction')} interaction
  */
 module.exports = async (interaction) => {
-	const { client } = interaction;
-
 	try {
+		logger.info(`[CMD HANDLER]: '${interaction.logInfo}' was executed by ${interaction.user.tag}${interaction.guildID ? ` | ${interaction.member.displayName}` : ''} in ${interaction.guildID ? `#${interaction.channel.name} | ${interaction.guild.name}` : 'DMs'}`);
+
 		/** @type {import('../structures/commands/SlashCommand')} */
-		const command = client.commands.get(interaction.commandName);
+		const command = interaction.client.commands.get(interaction.commandName);
 
 		if (!command) return;
 
@@ -25,8 +25,6 @@ module.exports = async (interaction) => {
 
 			// prevent from executing owner only command
 			if (command.category === 'owner') {
-				logger.info(`[CMD HANDLER]: ${interaction.user.tag}${interaction.guildID ? ` | ${interaction.member.displayName}` : ''} tried to execute '${interaction.logInfo}' in ${interaction.guildID ? `#${interaction.channel.name} | ${interaction.guild.name}` : 'DMs'} which is an owner only command`);
-
 				return await interaction.reply({
 					content: `the \`${command.name}\` command is only for the bot owners`,
 					ephemeral: true,
@@ -37,18 +35,14 @@ module.exports = async (interaction) => {
 		// command cooldowns
 		if (command.cooldown) {
 			const NOW = Date.now();
-			const COOLDOWN_TIME = (command.cooldown ?? client.config.getNumber('COMMAND_COOLDOWN_DEFAULT')) * 1000;
+			const COOLDOWN_TIME = (command.cooldown ?? interaction.client.config.getNumber('COMMAND_COOLDOWN_DEFAULT')) * 1000;
 
 			if (command.timestamps.has(interaction.user.id)) {
 				const EXPIRATION_TIME = command.timestamps.get(interaction.user.id) + COOLDOWN_TIME;
 
 				if (NOW < EXPIRATION_TIME) {
-					const TIME_LEFT = ms(EXPIRATION_TIME - NOW, { long: true });
-
-					logger.info(`[CMD HANDLER]: ${interaction.user.tag}${interaction.guildID ? ` | ${interaction.member.displayName}` : ''} tried to execute '${interaction.logInfo}' in ${interaction.guildID ? `#${interaction.channel.name} | ${interaction.guild.name}` : 'DMs'} ${TIME_LEFT} before the cooldown expires`);
-
-					return interaction.reply({
-						content: `\`${command.name}\` is on cooldown for another \`${TIME_LEFT}\``,
+					return await interaction.reply({
+						content: `\`${command.name}\` is on cooldown for another \`${ms(EXPIRATION_TIME - NOW, { long: true })}\``,
 						ephemeral: true,
 					});
 				}
@@ -58,24 +52,17 @@ module.exports = async (interaction) => {
 			setTimeout(() => command.timestamps.delete(interaction.user.id), COOLDOWN_TIME);
 		}
 
-		logger.info(`[CMD HANDLER]: '${interaction.logInfo}' was executed by ${interaction.user.tag}${interaction.guildID ? ` | ${interaction.member.displayName}` : ''} in ${interaction.guildID ? `#${interaction.channel.name} | ${interaction.guild.name}` : 'DMs'}`);
-
 		await command.run(interaction);
 	} catch (error) {
 		logger.error(error);
 
 		try {
-			if (typeof error === 'string') {
-				await interaction.reply({
-					content: `${error}`,
-					ephemeral: true,
-				});
-			} else {
-				await interaction.reply({
-					content: `an error occurred while executing the command: ${error}`,
-					ephemeral: true,
-				});
-			}
+			await interaction.reply({
+				content: typeof error === 'string'
+					? error
+					: `an error occurred while executing the command: ${error}`,
+				ephemeral: true,
+			});
 		} catch (err) {
 			logger.error(err);
 		}
