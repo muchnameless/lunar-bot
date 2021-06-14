aa7d8f668129df0662b07515e6f051230edada98
muchnameless <muchnameless@gmail.com>
2021-06-14 20:18:48 +0200

    fix(debug): response formatting

--- src/commands/owner/debug.js
+++ src/commands/owner/debug.js
@@ -26,22 +26,22 @@ module.exports = class DebugCommand extends SlashCommand {
 		const used = process.memoryUsage();
 
 		let response = stripIndents`
-			**General:**
+			General:
 			Ready at: ${this.client.readyAt.toLocaleString('de-DE', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' })}
 			Uptime: ${ms(this.client.uptime)}
 
-			**Discord:**
+			Discord:
 			Guilds: ${this.client.guilds.cache.size.toLocaleString(this.config.get('NUMBER_FORMAT'))}
 			Channels: ${this.client.channels.cache.size.toLocaleString(this.config.get('NUMBER_FORMAT'))}
 			Members: ${this.client.guilds.cache.reduce((acc, guild) => acc + guild.members.cache.size, 0).toLocaleString(this.config.get('NUMBER_FORMAT'))}
 			Users: ${this.client.users.cache.size.toLocaleString(this.config.get('NUMBER_FORMAT'))}
 			Discord.js v${version}
 
-			**Memory:**
+			Memory:
 		`;
 
 		for (const [ key, value ] of Object.entries(used)) {
-			response += `${key} ${Math.round(value / 1024 / 1024 * 100) / 100} MB\n`;
+			response += `\n${key}: ${Math.round(value / 1024 / 1024 * 100) / 100} MB`;
 		}
 
 		return interaction.reply({
