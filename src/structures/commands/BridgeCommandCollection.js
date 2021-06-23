1be9b43ac666370988eb48e7b63d085f56708b3f
muchnameless <muchnameless@gmail.com>
2021-06-24 01:39:01 +0200

    fix(BridgeCommandCollection#help): fix missing port to bridge commands

--- src/structures/commands/BridgeCommandCollection.js
+++ src/structures/commands/BridgeCommandCollection.js
@@ -64,9 +64,9 @@ module.exports = class BridgeCommandCollection extends BaseCommandCollection {
 	 */
 	async help(message, args) {
 		try {
-			return await this.get('help').run(message, args);
+			return await this.get('help').runInGame(message, args);
 		} catch (error) {
-			logger.error(`[CMD HANDLER]: An error occured while ${message.author.tag}${message.guild ? ` | ${message.member.displayName}` : ''} tried to execute ${message.content} in ${message.guild ? `#${message.channel.name} | ${message.guild}` : 'DMs'}`, error);
+			logger.error(`[CMD HANDLER]: An error occured while ${message.author.ign} tried to execute '${message.content}' in '${message.type}'`, error);
 			message.reply(`an error occured while executing the \`help\` command:\n${error}`);
 		}
 	}
