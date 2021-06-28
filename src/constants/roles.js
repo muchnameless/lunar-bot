8f66cb8800e412dd4a1db0b4564e3deeeb4bb1f7
muchnameless <muchnameless@gmail.com>
2021-06-28 19:00:03 +0200

    refactor(constants.roles): remove skill below 50, cata below 30

--- src/constants/roles.js
+++ src/constants/roles.js
@@ -3,13 +3,13 @@
 module.exports = {
 	skillAverageRoles: [ 40, 45, 50, 55, 60 ],
 
-	skillRoles: [ 45, 50, 55, 60 ],
+	skillRoles: [ 50, 55, 60 ],
 
 	slayerTotalRoles: [ 7, 8, 9 ],
 
 	slayerRoles: [ 8, 9 ],
 
-	catacombsRoles: [ 20, 25, 30, 35, 40, 45, 50 ],
+	catacombsRoles: [ 30, 35, 40, 45, 50 ],
 
 	delimiterRoles: [ 'GUILD', 'SKILL', 'SLAYER', 'DUNGEON', 'MISC' ],
 };
