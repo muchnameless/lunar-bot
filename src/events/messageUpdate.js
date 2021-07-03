6df39bda73470ace67772b78a541ddbf03da5357
muchnameless <muchnameless@gmail.com>
2021-07-03 05:08:33 +0200

    refactor(events.messageUpdate): ignore messages older than a day

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -19,6 +19,7 @@ module.exports = class MessageUpdateEvent extends MessageEvent {
 	 */
 	async run(oldMessage, newMessage) {
 		if (oldMessage.content === newMessage.content) return; // pin or added embed
+		if (Date.now() - newMessage.createdTimestamp >= 24 * 60 * 60_000) return; // original message is older than a day
 
 		if (newMessage.me) this.client.chatBridges.handleDiscordMessage(newMessage, { checkifNotFromBot: false });
 
