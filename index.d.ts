c150bdce54a7d82608b7c081a91b7829c530c346
muchnameless <muchnameless@gmail.com>
2021-07-06 02:52:55 +0200

    types(*): module overwrites require('d.js') types

--- index.d.ts
+++ index.d.ts
@@ -1,31 +1,35 @@
-declare module 'discord.js' {
-	interface Message {
-		author: import('./src/structures/extensions/User');
-	}
-
-	interface User {
-		player: import('./src/structures/database/models/Player');
-	}
-
-	interface Channel {
-		botPermissions: Permissions;
-	}
-
-	interface GuildMember {
-		player: import('./src/structures/database/models/Player');
-		user: import('./src/structures/extensions/User');
-	}
-
-	interface ThreadMember {
-		player: import('./src/structures/database/models/Player');
-		user: import('./src/structures/extensions/User');
-	}
-
-	interface Base {
-		client: import('./src/structures/LunarClient');
-	}
-
-	interface MessageEmbed {
-		padFields(numberOfRows?: number): this;
-	}
+export class Message extends (await import('discord.js')).Message {
+	author: import('./src/structures/extensions/User');
 }
+
+// declare module 'discord.js' {
+// 	interface Message {
+// 		author: import('./src/structures/extensions/User');
+// 	}
+
+// 	interface User {
+// 		player: import('./src/structures/database/models/Player');
+// 	}
+
+// 	interface Channel {
+// 		botPermissions: Permissions;
+// 	}
+
+// 	interface GuildMember {
+// 		player: import('./src/structures/database/models/Player');
+// 		user: import('./src/structures/extensions/User');
+// 	}
+
+// 	interface ThreadMember {
+// 		player: import('./src/structures/database/models/Player');
+// 		user: import('./src/structures/extensions/User');
+// 	}
+
+// 	interface Base {
+// 		client: import('./src/structures/LunarClient');
+// 	}
+
+// 	interface MessageEmbed {
+// 		padFields(numberOfRows?: number): this;
+// 	}
+// }
