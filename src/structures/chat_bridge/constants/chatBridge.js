e65f36be09c517e460b583115c877cf229500827
muchnameless <muchnameless@gmail.com>
2021-07-15 02:40:31 +0200

    feat(blockedWordsRegExp)

--- src/structures/chat_bridge/constants/chatBridge.js
+++ src/structures/chat_bridge/constants/chatBridge.js
@@ -49,7 +49,7 @@ module.exports = {
 	 */
 	defaultResponseRegExp: /[^-\s\u{2003}\u{2800}\u{0020}\u{180E}\u{200B}]/u,
 
-	blockedWordsRegExp: /\bsex\b|\bcum\b|\bsemen\b|nutte(?:d|r)|\bjerk +of|suck +me +off|\bboner\b|schl[o0]ng|\borgy\b|\bth[o0]t\b|pussy|orgasm|\ba+n+a+l+\b|suck +my +dick|nude|\bp+ornhub+(?:\..+)?\b|\bxhamster(?:\..+)?\b|\bpedo(?:phile|s)?\b|(?:you'?r+?e?|ur|(?:your|ur) +(?:m[uo]m|dad)|\bs?he +is) +gay+\b|fa(g+o+|g{2,})t|f(?:4|@)g|cocain|\bh+e+r+o+i+n+\b|\bk+ys+\b|(?:kill.+?|sho{2,}t+) +y *o *u *r *s *e *l *f+\b|school *shoot|get +aids|autist|\bsuicide\b|\bslave\b|brain *dead|retarted|\bn+i[bg]{2,}(?:er+|a+)\b|\bniga*\b|\bchang\b|\bcoon+\b|\bhurri\b|\bk{3,}\b|\br+ape+\b|kkr|hope +you +die|die +tmr|go +die +in +a +hole|\bcok\b/i,
+	blockedWordsRegExp: /\bsex\b|\bcum\b|\bsemen\b|\br+ape+d*\b|nutte(?:d|r)|\bjerk +of|suck +me +off|\bboner\b|schl[o0]ng|\borgy\b|\bth[o0]t\b|pussy|orgasm|\ba+n+a+l+\b|suck +my +dick|nude|\bp+ornhub+(?:\..+)?\b|\bxhamster(?:\..+)?\b|\bpedo(?:phile|s)?\b|(?:you'?r+?e?|ur|(?:your|ur) +(?:m[uo]m|dad)|\bs?he +is) +gay+\b|fa(g+o+|g{2,})t|f(?:4|@)g|cocain|\bh+e+r+o+i+n+\b|\bk+ys+\b|(?:kill.+?|sho{2,}t+) +y *o *u *r *s *e *l *f+\b|school *shoot|get +aids|autist|\bsuicide\b|\bslave\b|brain *dead|retarted|\bn+i[bg]{2,}(?:er+|a+)\b|\bniga*\b|\bchang\b|\bcoon+\b|\bhurri\b|\bk{3,}\b|\br+ape+\b|kkr|hope +you +die|die +tmr|go +die +in +a +hole|\bcok\b/i,
 
 	memeRegExp: /[⠁-⣿]/,
 
