9e9c5d27ee3d34dbdbfcc2e17a91d58197864c9c
muchnameless <muchnameless@gmail.com>
2021-07-25 01:41:14 +0200

    refactor(interactionCreate): error handling

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -20,69 +20,52 @@ module.exports = class InteractionCreateEvent extends Event {
 	 * @param {import('../structures/extensions/CommandInteraction')} interaction
 	 */
 	async _handleCommandInteraction(interaction) {
-		try {
-			logger.info(`[CMD HANDLER]: '${interaction.logInfo}' was executed by ${interaction.user.tag}${interaction.guildId ? ` | ${interaction.member.displayName}` : ''} in ${interaction.guildId ? `#${interaction.channel.name} | ${interaction.guild.name}` : 'DMs'}`);
+		logger.info(`[CMD HANDLER]: '${interaction.logInfo}' was executed by ${interaction.user.tag}${interaction.guildId ? ` | ${interaction.member.displayName}` : ''} in ${interaction.guildId ? `#${interaction.channel.name} | ${interaction.guild.name}` : 'DMs'}`);
 
-			/** @type {import('../structures/commands/SlashCommand')} */
-			const command = this.client.commands.get(interaction.commandName);
+		/** @type {import('../structures/commands/SlashCommand')} */
+		const command = this.client.commands.get(interaction.commandName);
 
-			if (!command) return await interaction.reply({
-				content: `${interaction.commandName} is currently disabled`,
-				ephemeral: true,
-			});
+		if (!command) return interaction.reply({
+			content: `${interaction.commandName} is currently disabled`,
+			ephemeral: true,
+		});
 
-			if (interaction.user.id !== this.client.ownerId) {
-				interaction.defer();
+		if (interaction.user.id !== this.client.ownerId) {
+			interaction.defer();
 
-				// role permissions
-				await command.checkPermissions(interaction);
+			// role permissions
+			await command.checkPermissions(interaction);
 
-				// prevent from executing owner only command
-				if (command.category === 'owner') {
-					return await interaction.reply({
-						content: `the \`${command.name}\` command is only for the bot owners`,
+			// prevent from executing owner only command
+			if (command.category === 'owner') {
+				return interaction.reply({
+					content: `the \`${command.name}\` command is restricted to the bot owner`,
+					ephemeral: true,
+				});
+			}
+		}
+
+		// command cooldowns
+		if (command.cooldown !== 0) {
+			const NOW = Date.now();
+			const COOLDOWN_TIME = (command.cooldown ?? this.config.get('COMMAND_COOLDOWN_DEFAULT')) * 1000;
+
+			if (command.timestamps.has(interaction.user.id)) {
+				const EXPIRATION_TIME = command.timestamps.get(interaction.user.id) + COOLDOWN_TIME;
+
+				if (NOW < EXPIRATION_TIME) {
+					return interaction.reply({
+						content: `\`${command.name}\` is on cooldown for another \`${ms(EXPIRATION_TIME - NOW, { long: true })}\``,
 						ephemeral: true,
 					});
 				}
 			}
 
-			// command cooldowns
-			if (command.cooldown !== 0) {
-				const NOW = Date.now();
-				const COOLDOWN_TIME = (command.cooldown ?? this.config.get('COMMAND_COOLDOWN_DEFAULT')) * 1000;
-
-				if (command.timestamps.has(interaction.user.id)) {
-					const EXPIRATION_TIME = command.timestamps.get(interaction.user.id) + COOLDOWN_TIME;
-
-					if (NOW < EXPIRATION_TIME) {
-						return await interaction.reply({
-							content: `\`${command.name}\` is on cooldown for another \`${ms(EXPIRATION_TIME - NOW, { long: true })}\``,
-							ephemeral: true,
-						});
-					}
-				}
-
-				command.timestamps.set(interaction.user.id, NOW);
-				setTimeout(() => command.timestamps.delete(interaction.user.id), COOLDOWN_TIME);
-			}
-
-			await command.run(interaction);
-		} catch (error) {
-			logger.error(error);
-
-			if (error.code === Constants.APIErrors.INVALID_WEBHOOK_TOKEN) return; // interaction expired
-
-			try {
-				await interaction.reply({
-					content: typeof error === 'string'
-						? error
-						: `an error occurred while executing the command: ${error}`,
-					ephemeral: true,
-				});
-			} catch (err) {
-				logger.error(err);
-			}
+			command.timestamps.set(interaction.user.id, NOW);
+			setTimeout(() => command.timestamps.delete(interaction.user.id), COOLDOWN_TIME);
 		}
+
+		return command.run(interaction);
 	}
 
 	/**
@@ -109,13 +92,30 @@ module.exports = class InteractionCreateEvent extends Event {
 	 * @param {import('discord.js').Interaction} interaction
 	 */
 	async run(interaction) {
-		// commands
-		if (interaction.isCommand()) return this._handleCommandInteraction(interaction);
+		try {
+			// commands
+			if (interaction.isCommand()) return await this._handleCommandInteraction(interaction);
 
-		// buttons
-		if (interaction.isButton()) return this._handleButtonInteraction(interaction);
+			// buttons
+			if (interaction.isButton()) return await this._handleButtonInteraction(interaction);
 
-		// select menus
-		if (interaction.isSelectMenu()) return this._handleSelectMenuInteraction(interaction);
+			// select menus
+			if (interaction.isSelectMenu()) return await this._handleSelectMenuInteraction(interaction);
+		} catch (error) {
+			logger.error(error);
+
+			if (error.code === Constants.APIErrors.INVALID_WEBHOOK_TOKEN) return; // interaction expired
+
+			try {
+				await interaction.reply({
+					content: typeof error === 'string'
+						? error
+						: `an error occurred while executing the command: ${error}`,
+					ephemeral: true,
+				});
+			} catch (err) {
+				logger.error(err);
+			}
+		}
 	}
 };
