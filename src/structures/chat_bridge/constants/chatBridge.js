3e7259cd37cd532833eb0102cb0027ac1e3fcd7f
muchnameless <muchnameless@gmail.com>
2021-07-28 15:38:48 +0200

    feat(blockedWordsRegExp)

--- src/structures/chat_bridge/constants/chatBridge.js
+++ src/structures/chat_bridge/constants/chatBridge.js
@@ -49,7 +49,7 @@ module.exports = {
 	 */
 	defaultResponseRegExp: /[^-\s\u{2003}\u{2800}\u{0020}\u{180E}\u{200B}]/u,
 
-	blockedWordsRegExp: /\bs[e3]x\b|seks|\bcum\b|\bsemen\b|\br+ape+d*\b|nutte(?:d|r)|\bjerk +of|suck +me +off|\bb[o0]n[e3]r\b|schl[o0]ng|\borgy\b|\bth[o0]t\b|pus{2,}y|orgasm|\ba+n+a+l+\b|suck +my +dick|nude|\bp+ornhub+(?:\..+)?\b|\bxhamster(?:\..+)?\b|\bp[e3]do(?:phile|s)?\b|(?:you'?r+?e?|ur|(?:your|ur) +(?:m[uo]m|dad)|\bs?he +is) +gay+\b|fa(g+o+|g{2,})t|f(?:4|@)g|cocain|\bh+e+r+o+i+n+\b|\bk+ys+\b|(?:kill.*|sho{2,}t+) +y *o *u *r *s *e *l *f+\b|kil{2,}m+y+ *s+elf|school *shoot|get +aids|autist|\bsuicide\b|\bslave\b|brain *dead|retarted|deranged|\bn+i[bg]{2,}(?:er+|a+)\b|\bniga*\b|\bch[ai]ng\b|\bco{2,}n+s*\b|\bhurri\b|\bk{3,}\b|\br+ape+\b|kkr|hope +you +die|die +tmr|go +die +in +a +hole|\bcok\b|kanker/i,
+	blockedWordsRegExp: /\bs[e3]x\b|seks|\bcum\b|\bsemen\b|\br+ape+d*\b|nutte(?:d|r)|\bjerk +of|suck +me +off|\bb[o0]n[e3]r\b|schl[o0]ng|\borgy\b|\bth[o0]t\b|pus{2,}y|orgasm|\ba+n+a+l+\b|suck +my +dick|nude|\bp+ornhub+(?:\..+)?\b|\bxhamster(?:\..+)?\b|\bp[e3]do(?:phile|s)?\b|(?:you'?r+?e?|ur|(?:your|ur) +(?:m[uo]m|dad)|\bs?he +is) +gay+\b|fa(g+o+|g{2,})t|f(?:4|@)g|cocain|\bh+e+r+o+i+n+\b|\bk+ys+\b|(?:kill.*|sho{2,}t+) +y *o *u *r *s *e *l *f+\b|kil{2,}m+y+ *s+elf|school *shoot|get +aids|autist|\bsuicide\b|\bslave\b|brain *dead|retarted|deranged|\bn+i(?:b{2,}|g{2,})(?:er+|a+)\b|\bnig+a*\b|\bch[ai]ng\b|\bco{2,}n+s*\b|\bhurri\b|\bk{3,}\b|\br+ape+\b|kkr|hope +you +die|die +tmr|go +die +in +a +hole|\bcok\b|kanker/i,
 
 	memeRegExp: /[⠁-⣿]/,
 
