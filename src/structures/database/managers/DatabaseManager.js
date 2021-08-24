7063f13e316df4bc9369a14ef7e3be463071993a
muchnameless <muchnameless@gmail.com>
2021-08-24 02:45:33 +0200

    refactor(DatabaseManager): update logic

--- src/structures/database/managers/DatabaseManager.js
+++ src/structures/database/managers/DatabaseManager.js
@@ -16,7 +16,7 @@ import { TaxCollector } from '../models/TaxCollector.js';
 import { ModelManager } from './ModelManager.js';
 import { ChatTrigger } from '../models/ChatTrigger.js';
 import { ChannelUtil } from '../../../util/index.js';
-import { asyncFilter, logger, safePromiseAll } from '../../../functions/index.js';
+import { asyncFilter, compareAlphabetically, logger } from '../../../functions/index.js';
 
 
 export class DatabaseManager {
@@ -158,120 +158,158 @@ export class DatabaseManager {
 	}
 
 	/**
-	 * updates the tax database
-	 * @returns {Promise<string[]>}
+	 * updates the tax database, returns availableAuctions per taxCollector
 	 */
 	async #updateTaxDatabase() {
 		const { config, players, taxCollectors } = this.modelManagers;
 		const TAX_AUCTIONS_START_TIME = config.get('TAX_AUCTIONS_START_TIME');
 		const TAX_AMOUNT = config.get('TAX_AMOUNT');
 		const TAX_AUCTIONS_ITEMS = config.get('TAX_AUCTIONS_ITEMS');
-		const NOW = Date.now();
 		const availableAuctionsLog = [];
-		const taxPaidLog = [];
+		const dbPromises = [];
 
-		let auctionsAmount = 0;
-		let unknownPlayers = 0;
 		let apiError = false;
 
 		// update db
 		for (const taxCollector of taxCollectors.cache.values()) {
-			if (!taxCollector.isCollecting) continue;
+			if (!taxCollector.isCollecting) continue; // skip retired collectors
 
-			if (apiError) {
-				availableAuctionsLog.push(`\u200b > ${taxCollector}: API Error`);
+			if (apiError) { // skip the rest if an API error occurred
+				availableAuctionsLog.push({
+					ign: taxCollector.ign,
+					auctions: 'API Error',
+				});
+
 				continue;
 			}
 
 			try {
 				const auctions = await hypixel.skyblock.auction.player(taxCollector.minecraftUuid);
+
+				// collector has no auctions in the API
+				if (!auctions.length) {
+					availableAuctionsLog.push({
+						ign: taxCollector.ign,
+						auctions: 0,
+					});
+
+					continue;
+				}
+
+				/** @type {import('@zikeji/hypixel').Components.Schemas.SkyBlockAuction[]} */
 				const taxAuctions = [];
-				const paidLog = [];
 
 				let availableAuctions = 0;
 
+				// filter auctions
 				for (const auction of await asyncFilter(
 					auctions,
 					// correct item & started after last reset & no outbid from already logged auction
 					auc => TAX_AUCTIONS_ITEMS.includes(auc.item_name) && auc.start >= TAX_AUCTIONS_START_TIME && this.#validateAuctionId(auc.uuid),
-				)) auction.highest_bid_amount >= TAX_AMOUNT
-					? auction.bids.length && taxAuctions.push(auction)
-					: auction.end > NOW && ++availableAuctions;
+				)) {
+					if (auction.highest_bid_amount >= TAX_AMOUNT) {
+						if (auction.bids.length) taxAuctions.push(auction); // player bid on the auction
+					} else if (auction.end > Date.now()) { // auction not expired
+						++availableAuctions;
+					}
+				}
 
-				availableAuctionsLog.push(`\u200b > ${taxCollector}: ${availableAuctions}`);
-
-				auctionsAmount += taxAuctions.length;
-
-				await safePromiseAll(taxAuctions.map(async (auction) => {
-					const { bidder, amount } = auction.bids.at(-1);
-					const player = players.cache.get(bidder);
-
-					if (!player) return ++unknownPlayers;
-
-					paidLog.push(`${player}: ${this.client.formatNumber(amount)}`);
-
-					return player.setToPaid({
-						amount,
-						collectedBy: taxCollector.minecraftUuid,
-						auctionId: auction.uuid,
-						shouldAdd: true,
-					});
-				}));
-
-				if (!paidLog.length) continue;
-
-				taxPaidLog.push({
-					name: `/ah ${taxCollector}`,
-					value: Formatters.codeBlock(paidLog.join('\n')),
+				availableAuctionsLog.push({
+					ign: taxCollector.ign,
+					auctions: availableAuctions,
 				});
+
+				if (taxAuctions.length) dbPromises.push((async () => {
+					const paidLog = [];
+
+					// update database
+					await Promise.all(taxAuctions.map(async (auction) => {
+						const { bidder, amount } = auction.bids.at(-1);
+						const player = players.cache.get(bidder);
+
+						if (!player) return;
+
+						try {
+							await player.setToPaid({
+								amount,
+								collectedBy: taxCollector.minecraftUuid,
+								auctionId: auction.uuid,
+								shouldAdd: true,
+							});
+
+							paidLog.push(`${player}: ${this.client.formatNumber(amount)}`);
+						} catch (error) {
+							logger.error(error);
+							paidLog.push(`${player}: ${error}`);
+						}
+					}));
+
+					// logging
+					if (paidLog.length) return {
+						name: `/ah ${taxCollector}`,
+						value: Formatters.codeBlock(paidLog.join('\n')),
+					};
+				})());
 			} catch (error) {
 				logger.error(`[UPDATE TAX DB]: ${taxCollector}`, error);
 				apiError = true;
-				availableAuctionsLog.push(`\u200b > ${taxCollector}: API Error`);
+				availableAuctionsLog.push({
+					ign: taxCollector.ign,
+					auctions: 'API Error',
+				});
 			}
 		}
 
-		// logging
-		if (auctionsAmount && unknownPlayers && new Date().getMinutes() < config.get('DATABASE_UPDATE_INTERVAL')) {
-			logger.info(`[UPDATE TAX DB]: New auctions: ${auctionsAmount}, unknown players: ${unknownPlayers}`);
-		}
+		// update database
+		if (dbPromises.length) {
+			setTimeout((() => async () => {
+				const taxPaidLog = (await Promise.all(dbPromises)).filter(x => x != null);
 
-		if (taxPaidLog.length) {
-			this.client.log(this.client.defaultEmbed
-				.setTitle('Guild Tax')
-				.addFields(...taxPaidLog),
-			);
+				// logging
+				if (taxPaidLog.length) {
+					this.client.log(this.client.defaultEmbed
+						.setTitle('Guild Tax')
+						.addFields(...taxPaidLog),
+					);
+				}
+			})(), 0);
 		}
 
 		return availableAuctionsLog
-			.sort((a, b) => a.split(':')[0].toLowerCase().localeCompare(b.split(':')[0].toLowerCase())) // alphabetically
-			.sort((a, b) => Number(b.split(':')[1]) - Number(a.split(':')[1])); // number of auctions
+			.sort((a, b) => compareAlphabetically(a.ign, b.ign)) // alphabetically
+			.sort((a, b) => b.auctions - a.auctions) // number of auctions
+			.map(({ ign, auctions }) => `\u200b > ${ign}: ${auctions}`);
 	}
 
 	/**
-	 * creates and returns a tax embed
+	 * tax embed description
 	 * @param {?string[]} availableAuctionsLog
 	 */
-	createTaxEmbed(availableAuctionsLog = null) {
-		const { config, hypixelGuilds, players, taxCollectors } = this.modelManagers;
+	#createTaxEmbedDescription(availableAuctionsLog = null) {
+		const { config, players, taxCollectors } = this.modelManagers;
 		const activeTaxCollectors = taxCollectors.activeCollectors; // eslint-disable-line no-shadow
 		const playersInGuild = players.inGuild;
 		const PLAYER_COUNT = playersInGuild.size;
 		const PAID_COUNT = playersInGuild.filter(({ paid }) => paid).size;
-		const TOTAL_COINS = taxCollectors.cache.reduce((acc, { collectedTax }) => acc + collectedTax, 0);
-		const taxEmbed = this.client.defaultEmbed
-			.setTitle('Guild Tax')
-			.setDescription(Formatters.codeBlock('cs', stripIndents(commaLists`
-				Collectors: # /ah ${activeTaxCollectors.map(player => player.ign).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))}
-				Amount: ${this.client.formatNumber(config.get('TAX_AMOUNT'))}
-				Items: ${config.get('TAX_AUCTIONS_ITEMS').map(item => `'${item}'`)}
-				Paid: ${PAID_COUNT} / ${PLAYER_COUNT} | ${Math.round((PAID_COUNT / PLAYER_COUNT) * 100)} % | collected amount: ${this.client.formatNumber(TOTAL_COINS)} coins
-				Available auctions:
-				${availableAuctionsLog?.join('\n') ?? '\u200b -'}
-			`)))
-			.setFooter('Last updated at');
+		const TOTAL_COINS = this.client.formatNumber(taxCollectors.cache.reduce((acc, { collectedTax }) => acc + collectedTax, 0));
 
-		// add guild specific fields
+		return Formatters.codeBlock('cs', stripIndents(commaLists`
+			Collectors: # /ah ${activeTaxCollectors.map(player => player.ign).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))}
+			Amount: ${this.client.formatNumber(config.get('TAX_AMOUNT'))}
+			Items: ${config.get('TAX_AUCTIONS_ITEMS').map(item => `'${item}'`)}
+			Paid: ${PAID_COUNT} / ${PLAYER_COUNT} | ${Math.round((PAID_COUNT / PLAYER_COUNT) * 100)} % | collected amount: ${TOTAL_COINS} coins
+			Available auctions:
+			${availableAuctionsLog?.join('\n') ?? '\u200b -'}
+		`));
+	}
+
+	/**
+	 * tax embed fields
+	 */
+	#createTaxEmbedFields() {
+		const { config, hypixelGuilds } = this.modelManagers;
+		const fields = [];
+
 		for (const hypixelGuild of hypixelGuilds.cache.values()) {
 			const GUILD_PLAYER_COUNT = hypixelGuild.playerCount;
 			const ENTRIES_PER_ROW = Math.ceil(GUILD_PLAYER_COUNT / 3);
@@ -301,7 +339,7 @@ export class DatabaseManager {
 					paddedValue += '\n\u200b';
 				}
 
-				taxEmbed.addFields({
+				fields.push({
 					name: index % 2
 						? `${hypixelGuild.name} (${GUILD_PLAYER_COUNT})`
 						: '\u200b',
@@ -311,7 +349,20 @@ export class DatabaseManager {
 			}
 		}
 
-		return taxEmbed;
+		return fields;
+	}
+
+	/**
+	 * creates and returns a tax embed
+	 * @param {string} [description]
+	 * @param {import('discord.js').EmbedField[]} [fields]
+	 */
+	createTaxEmbed(description = this.#createTaxEmbedDescription(), fields = this.#createTaxEmbedFields()) {
+		return this.client.defaultEmbed
+			.setTitle('Guild Tax')
+			.setDescription(description)
+			.addFields(...fields)
+			.setFooter('Last updated at');
 	}
 
 	/**
@@ -319,18 +370,21 @@ export class DatabaseManager {
 	 * @param {import('../../LunarClient').LunarClient} client
 	 */
 	async updateData() {
-		const { config, players } = this.modelManagers;
+		const { config, players, hypixelGuilds } = this.modelManagers;
 
 		// the hypxiel api encountered an error before
 		if (config.get('HYPIXEL_API_ERROR')) {
 			// reset error every full hour
-			if (new Date().getMinutes() >= config.get('DATABASE_UPDATE_INTERVAL')) return logger.warn('[DB UPDATE]: auto updates disabled');
+			if (new Date().getMinutes() >= config.get('DATABASE_UPDATE_INTERVAL')) {
+				players.updateIgn();
+				return logger.warn('[DB UPDATE]: auto updates disabled');
+			}
 
 			config.set('HYPIXEL_API_ERROR', false);
 		}
 
 		// update player db
-		await this.modelManagers.hypixelGuilds.updateData({ syncRanks: true });
+		await hypixelGuilds.updateData({ syncRanks: true });
 
 		// update tax db
 		const availableAuctionsLog = config.get('TAX_TRACKING_ENABLED')
@@ -340,6 +394,7 @@ export class DatabaseManager {
 		// update Xp
 		if (config.get('XP_TRACKING_ENABLED')) players.updateXp();
 
+		// update IGNs
 		await players.updateIgn();
 
 		// update taxMessage
@@ -351,17 +406,12 @@ export class DatabaseManager {
 			return logger.warn('[TAX MESSAGE]: missing permission to edit taxMessage');
 		}
 
-		const taxEmbed = this.createTaxEmbed(availableAuctionsLog);
 		const TAX_MESSAGE_ID = config.get('TAX_MESSAGE_ID');
 		const taxMessage = TAX_MESSAGE_ID && await taxChannel.messages.fetch(TAX_MESSAGE_ID).catch(error => logger.error('[TAX MESSAGE]', error));
 
-		if (!taxMessage?.editable || taxMessage.deleted) { // taxMessage deleted
+		if (!taxMessage?.editable || taxMessage.deleted) { // taxMessage deleted -> send a new one
 			try {
-				const { id } = await taxChannel.send({
-					embeds: [
-						taxEmbed,
-					],
-				});
+				const { id } = await taxChannel.send({ embeds: [ this.createTaxEmbed(this.#createTaxEmbedDescription(availableAuctionsLog)) ] });
 
 				config.set('TAX_MESSAGE_ID', id);
 				return logger.info('[TAX MESSAGE]: created new taxMessage');
@@ -370,17 +420,16 @@ export class DatabaseManager {
 			}
 		}
 
-		if (taxMessage.embeds[0]?.description === taxEmbed.description
+		const description = this.#createTaxEmbedDescription(availableAuctionsLog);
+		const fields = this.#createTaxEmbedFields();
+
+		if (taxMessage.embeds[0]?.description === description
 			&& taxMessage.embeds[0].fields
-				.every(({ name, value }, index) => taxEmbed.fields[index].name === name && taxEmbed.fields[index].value === value)
+				.every(({ name, value }, index) => fields[index].name === name && fields[index].value === value)
 		) return; // no changes to taxMessage
 
 		try {
-			await taxMessage.edit({
-				embeds: [
-					taxEmbed,
-				],
-			});
+			await taxMessage.edit({ embeds: [ this.createTaxEmbed(description, fields) ] });
 
 			logger.info('[TAX MESSAGE]: updated taxMessage');
 		} catch (error) {
