c33a6f1a6453173b36369d6d09d91d4a05fd49d3
muchnameless <muchnameless@gmail.com>
2021-09-07 23:16:34 +0200

    refactor(index): add GUILD_MEMBER, USER partial; GUILD_EMOJIS_AND_STICKERS intent

--- src/index.js
+++ src/index.js
@@ -54,11 +54,11 @@ const client = new LunarClient({
 	}),
 	allowedMentions: { parse: [ 'users' ], repliedUser: true },
 	partials: [
-		Constants.PartialTypes.CHANNEL,
-		// Constants.PartialTypes.GUILD_MEMBER,
+		Constants.PartialTypes.CHANNEL, // DM channels
+		Constants.PartialTypes.GUILD_MEMBER,
 		Constants.PartialTypes.MESSAGE,
-		Constants.PartialTypes.REACTION,
-		// Constants.PartialTypes.USER,
+		Constants.PartialTypes.REACTION, // reactions on uncached messages
+		Constants.PartialTypes.USER,
 	],
 	failIfNotExists: false,
 	presence: {
@@ -69,17 +69,17 @@ const client = new LunarClient({
 		status: 'online',
 	},
 	intents: [
-		Intents.FLAGS.DIRECT_MESSAGES,
+		Intents.FLAGS.DIRECT_MESSAGES, // eval edit button
 		// Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
 		// Intents.FLAGS.DIRECT_MESSAGE_TYPING,
-		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILDS, // to populate guild and channel caches
 		// Intents.FLAGS.GUILD_BANS,
-		// Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+		Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, // to keep the cache updated for the chat bridge
 		// Intents.FLAGS.GUILD_INTEGRATIONS,
 		// Intents.FLAGS.GUILD_INVITES,
-		Intents.FLAGS.GUILD_MEMBERS,
-		Intents.FLAGS.GUILD_MESSAGES,
-		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.GUILD_MEMBERS, // guildMemberAdd, remove
+		Intents.FLAGS.GUILD_MESSAGES, // chat bridge
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS, // forward announcements to guild chat
 		// Intents.FLAGS.GUILD_MESSAGE_TYPING,
 		// Intents.FLAGS.GUILD_PRESENCES,
 		// Intents.FLAGS.GUILD_VOICE_STATES,
