7b415c43e14e0c610d5762a1aedecb7fea3371cd
muchnameless <muchnameless@gmail.com>
2022-01-28 20:36:43 +0100

    refactor(deploy): use Client#rest

--- src/scripts/deploy.ts
+++ src/scripts/deploy.ts
@@ -1,6 +1,5 @@
 import process from 'node:process';
-import { REST } from '@discordjs/rest';
-import { Routes, APIVersion } from 'discord-api-types/v9';
+import { Routes } from 'discord-api-types/v9';
 import { db } from '../structures/database';
 import { LunarClient } from '../structures/LunarClient';
 import { logger } from '../functions';
@@ -9,12 +8,13 @@ import type {
 	RESTPutAPIGuildApplicationCommandsPermissionsJSONBody,
 } from 'discord-api-types/v9';
 
-const rest = new REST({ version: APIVersion }).setToken(process.env.DISCORD_TOKEN!);
 const [, , GUILD_ID] = process.argv;
 const client = new LunarClient({
 	db,
 	intents: 0,
 });
+
+client.rest.setToken(process.env.DISCORD_TOKEN!);
 
 try {
 	logger.info('[DEPLOY]: initialising database');
@@ -30,7 +30,7 @@ try {
 
 	logger.info(`[DEPLOY]: started refreshing slash commands for ${GUILD_ID ? `guild ${GUILD_ID}` : 'the application'}`);
 
-	const apiCommands = (await rest.put(
+	const apiCommands = (await client.rest.put(
 		GUILD_ID
 			? Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID!, GUILD_ID)
 			: Routes.applicationCommands(process.env.DISCORD_CLIENT_ID!),
@@ -71,7 +71,7 @@ try {
 
 				logger.info(`[DEPLOY]: setting permissions for '${guildId}'`);
 
-				await rest.put(Routes.guildApplicationCommandsPermissions(process.env.DISCORD_CLIENT_ID!, guildId), {
+				await client.rest.put(Routes.guildApplicationCommandsPermissions(process.env.DISCORD_CLIENT_ID!, guildId), {
 					body: fullPermissions,
 				});
 
