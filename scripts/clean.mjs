06247912bc59f27426281fe689a8f05e403070b8
muchnameless <muchnameless@gmail.com>
2022-02-15 18:44:59 +0100

    chore: clean script

---
+++ scripts/clean.mjs
@@ -0,0 +1,7 @@
+import { URL } from 'node:url';
+import { rm } from 'node:fs/promises';
+
+const rootFolder = new URL('..', import.meta.url);
+const distFolder = new URL('./dist', rootFolder);
+
+await rm(distFolder, { recursive: true, force: true });
