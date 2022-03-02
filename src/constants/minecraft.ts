9d568fcea6adfa32c3531280b088ba76dd740b03
muchnameless <muchnameless@gmail.com>
2022-03-02 03:13:15 +0100

    revert: mc version bump

--- src/constants/minecraft.ts
+++ src/constants/minecraft.ts
@@ -3,7 +3,7 @@ import minecraftDataLoader from 'minecraft-data';
 /**
  * mc client version
  */
-export const MC_CLIENT_VERSION = '1.18.2';
+export const MC_CLIENT_VERSION = '1.18';
 
 /**
  * minecraft data from the version the bridge bot uses
