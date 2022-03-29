42512b43d515c2c11bf3f23dde6e78812d43b723
muchnameless <muchnameless@gmail.com>
2022-03-29 22:57:35 +0200

    refactor(dbInit): sequelize import

--- src/scripts/dbInit.ts
+++ src/scripts/dbInit.ts
@@ -1,18 +1,18 @@
 import { argv, exit } from 'node:process';
 import '../structures/database/pgEnum-fix'; // to allow `sync --alter` with pg enums set
-import { db } from '../structures/database';
+import { sequelize } from '../structures/database';
 import { logger } from '../functions';
 
 // @ts-expect-error
-db.sequelize.options.logging = (...x) => logger.debug(...x);
+sequelize.options.logging = (...x) => logger.debug(...x);
 
 const force = argv.includes('--force') || argv.includes('-f');
 const alter = argv.includes('--alter') || argv.includes('-a');
 
 try {
-	await db.sequelize.sync({ force, alter });
+	await sequelize.sync({ force, alter });
 	logger.info('Database synced');
-	await db.sequelize.close();
+	await sequelize.close();
 	exit(0);
 } catch (error) {
 	logger.error(error);
