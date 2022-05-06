f6258d1ddc376a079fdfd798ffc99889aad20e4c
muchnameless <muchnameless@gmail.com>
2022-05-06 22:43:27 +0200

    fix: migration syntax

--- migrations/1651868070-skyblock_items.js
+++ migrations/1651868070-skyblock_items.js
@@ -1,8 +1,9 @@
 export async function up(sql) {
 	await sql.unsafe(`
-		ALTER TABLE skyblock_items (
-			ADD COLUMN category TEXT,
-			RENAME COLUMN conversion TO dungeon_conversion
-		)
+		ALTER TABLE skyblock_items
+		ADD COLUMN category TEXT;
+
+		ALTER TABLE skyblock_items
+		RENAME COLUMN conversion TO dungeon_conversion;
 	`);
 }
