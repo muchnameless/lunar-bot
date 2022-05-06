ced1e1e7df9608ece0c0618730c51478729b069f
muchnameless <muchnameless@gmail.com>
2022-05-06 10:53:45 +0200

    feat(networth): opal gemstone

--- src/structures/networth/constants/gemstones.ts
+++ src/structures/networth/constants/gemstones.ts
@@ -1,5 +1,20 @@
-export const GEMSTONES = new Set(['JADE', 'AMBER', 'TOPAZ', 'SAPPHIRE', 'AMETHYST', 'JASPER', 'RUBY']);
+export const GEMSTONES = new Set([
+	'AMBER', //
+	'AMETHYST',
+	'JADE',
+	'JASPER',
+	'OPAL',
+	'RUBY',
+	'SAPPHIRE',
+	'TOPAZ',
+]);
 
-export const SPECIAL_GEMSTONES = new Set(['UNIVERSAL', 'COMBAT', 'OFFENSIVE', 'DEFENSIVE', 'MINING']);
+export const SPECIAL_GEMSTONES = new Set([
+	'COMBAT', //
+	'DEFENSIVE',
+	'MINING',
+	'OFFENSIVE',
+	'UNIVERSAL',
+]);
 
 export const IGNORED_GEMSTONES = new Set(['unlocked_slots']);
