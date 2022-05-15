0b8ae1dd12f2a6cae6b95caabe137c71eeb5c43b
muchnameless <muchnameless@gmail.com>
2022-05-15 02:32:39 +0200

    types(shardDisconnect): import from d.js (was probably DOM type)

--- src/events/shardDisconnect.ts
+++ src/events/shardDisconnect.ts
@@ -1,6 +1,7 @@
 import { logger } from '../logger';
 import { exitProcess } from '../process';
 import { Event, type EventContext } from '../structures/events/Event';
+import type { CloseEvent } from 'discord.js';
 
 export default class ShardDisconnectEvent extends Event {
 	constructor(context: EventContext) {
