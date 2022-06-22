231ce2980ffee96d2cf79b443ed3fc3445693d56
muchnameless <muchnameless@gmail.com>
2022-06-23 00:26:00 +0200

    types(jobs): messages

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -3,7 +3,7 @@ import { itemUpgrades, prices, accessories } from '../structures/networth/prices
 import { logger } from '../logger';
 import { Job } from '../structures/jobs/Job';
 import { JobManager } from '../structures/jobs/JobManager';
-import type { ItemUpgrade } from '../structures/networth/prices';
+import type { LevelWithSilent } from 'pino';
 import type { ParsedSkyBlockItem } from './pricesAndPatchNotes';
 import type { LunarClient } from '../structures/LunarClient';
 
@@ -14,19 +14,32 @@ export const enum JobType {
 	SkyBlockPriceUpdate,
 }
 
+type WorkerMessage =
+	| {
+			op: JobType.HypixelForumLastGUIDUpdate;
+			d: number;
+	  }
+	| { op: JobType.LogMessage; d: { lvl: LevelWithSilent; args: [string] } }
+	| { op: JobType.SkyBlockItemUpdate; d: ParsedSkyBlockItem[] }
+	| { op: JobType.SkyBlockPriceUpdate; d: { itemId: string; price: number } };
+
 export const jobs = new JobManager();
 
+/**
+ * start jobs and add listeners for messages from worker threads
+ * @param client
+ */
 export function startJobs(client: LunarClient) {
 	jobs.add(
 		new Job(new URL('./pricesAndPatchNotes.js', import.meta.url), {
-			message(message: { op: JobType; d: any }) {
+			message(message: WorkerMessage) {
 				switch (message.op) {
 					case JobType.HypixelForumLastGUIDUpdate:
 						void client.config.set('HYPIXEL_FORUM_LAST_GUID', message.d);
 						break;
 
 					case JobType.LogMessage:
-						logger[message.d.lvl as 'info' | 'warn' | 'error'](...(message.d.args as [string]));
+						logger[message.d.lvl](...message.d.args);
 						break;
 
 					case JobType.SkyBlockItemUpdate:
@@ -35,9 +48,9 @@ export function startJobs(client: LunarClient) {
 						accessories.clear();
 						itemUpgrades.clear();
 
-						for (const { id, category, ...data } of message.d as ParsedSkyBlockItem[]) {
+						for (const { id, category, ...data } of message.d) {
 							if (category === 'ACCESSORY') accessories.add(id);
-							if (data.stars) itemUpgrades.set(id, data as ItemUpgrade);
+							if (data.stars) itemUpgrades.set(id, data);
 						}
 						break;
 
@@ -46,8 +59,12 @@ export function startJobs(client: LunarClient) {
 						break;
 
 					default: {
-						const e: never = message.op;
-						logger.error(`[JOBS] pricesAndPatchNotes: unknown message op '${e}'`);
+						const e: never = message;
+						if (typeof e === 'string') {
+							logger.error(`[JOBS] pricesAndPatchNotes: unknown message op '${e}'`);
+						} else {
+							logger.error(e, '[JOBS] pricesAndPatchNotes: unknown message op');
+						}
 					}
 				}
 			},
