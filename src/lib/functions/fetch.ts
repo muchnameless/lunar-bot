ff2a4bb3ce0e6c8095a23bbc3a6ba7bdbf3b30f9
muchnameless <muchnameless@gmail.com>
2022-08-04 12:28:57 +0200

    refactor: fetch -> request

--- src/lib/functions/fetch.ts
+++ src/lib/functions/fetch.ts
@@ -1,11 +1,11 @@
-import type { Response } from 'undici';
+import type { Dispatcher } from 'undici';
 
 /**
  * consumes the response body to help with gc
  * @param res
  */
-export async function consumeBody(res: Response) {
-	if (res.body === null) return;
+export async function consumeBody(res: Dispatcher.ResponseData) {
+	if (res.body.bodyUsed) return;
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	for await (const _chunk of res.body) {
 		// force consumption of body
--- src/lib/structures/errors/FetchError.ts
+++ src/lib/structures/errors/FetchError.ts
@@ -1,26 +1,27 @@
-import type { Response } from 'undici';
+import type { IncomingHttpHeaders } from 'node:http';
+import type { Dispatcher, Response } from 'undici';
 
 export class FetchError extends Error {
 	type: string | null;
 	url: string | null;
 	status: number | null;
 	statusText: string | null;
-	headers: [string, string][];
+	headers: IncomingHttpHeaders;
 
 	/**
 	 * @param name error name
 	 * @param response fetch response
 	 * @param message optional message to overwrite the default (fetch response statusText)
 	 */
-	constructor(name: string, { type, url, status, statusText, headers }: Partial<Response>, message?: string) {
+	constructor(name: string, { statusCode, opaque, context, headers }: Dispatcher.ResponseData, message?: string) {
 		super(message ?? statusText);
 
 		this.name = name;
 		this.type = type ?? null;
 		this.url = url ?? null;
-		this.status = status ?? null;
+		this.status = statusCode ?? null;
 		this.statusText = statusText ?? null;
-		this.headers = [...(headers?.entries() ?? [])];
+		this.headers = headers;
 	}
 
 	override toString() {
