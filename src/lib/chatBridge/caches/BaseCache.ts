b0831cf0437e358679919a912ecd5e2202398bb1
muchnameless <muchnameless@gmail.com>
2022-08-28 00:46:28 +0200

    refactor: use LimitedCollection in bridge caches

--- src/lib/chatBridge/caches/BaseCache.ts
+++ src/lib/chatBridge/caches/BaseCache.ts
@@ -1,8 +1,8 @@
-import { Collection } from 'discord.js';
+import { LimitedCollection } from 'discord.js';
 import type { Snowflake } from 'discord.js';
 
 export abstract class BaseCache<V, K = Snowflake> {
-	protected _cache = new Collection<K, V>();
+	protected _cache = new LimitedCollection<K, V>({ maxSize: 200 });
 	protected declare static _maxAge: number;
 
 	abstract get(id: K): V | null;
