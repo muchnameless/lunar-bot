6c2abbe5e800ba831c1ce1cfcb3c846a879acd54
muchnameless <muchnameless@gmail.com>
2022-08-31 23:22:09 +0200

    refactor(ChatBridge): login error handling

--- src/lib/chatBridge/events/error.ts
+++ src/lib/chatBridge/events/error.ts
@@ -10,11 +10,16 @@ export default class ErrorChatBridgeEvent extends ChatBridgeEvent {
 	override run(error: unknown) {
 		logger.error(error, '[CHATBRIDGE ERROR]');
 
-		if (error instanceof Error && /invalid credentials/i.test(error.message)) {
-			this.chatBridge.minecraft.state = MinecraftChatManagerState.Errored;
-			this.chatBridge.minecraft.disconnect();
+		if (error instanceof Error) {
+			// handle login errors
+			if (error.message.startsWith('Failed to obtain profile data')) {
+				void this.chatBridge.minecraft.reconnect();
+			} else if (error.message.startsWith('Invalid credentials')) {
+				this.chatBridge.minecraft.state = MinecraftChatManagerState.Errored;
+				this.chatBridge.minecraft.disconnect();
 
-			logger.error('[CHATBRIDGE ERROR]: invalid credentials detected');
+				logger.error('[CHATBRIDGE ERROR]: invalid credentials detected');
+			}
 		}
 	}
 }
