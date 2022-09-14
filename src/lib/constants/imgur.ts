5c1097e493cf1beb1d2c5728b38e78b0c90564b8
muchnameless <muchnameless@gmail.com>
2022-09-14 22:04:54 +0200

    fix(imgur): size limit

--- src/lib/constants/imgur.ts
+++ src/lib/constants/imgur.ts
@@ -1,7 +1,8 @@
 /**
- * 20 MB in Bytes, content-length header is in Bytes
+ * 10 MB in Bytes, content-length header is in Bytes (website allows up to 20 MB, but API is capped at 10)
+ * https://apidocs.imgur.com/#c85c9dfc-7487-4de2-9ecd-66f727cf3139
  */
-export const MAX_IMAGE_UPLOAD_SIZE = 20 * 1e6;
+export const MAX_IMAGE_UPLOAD_SIZE = 10 * 1e6;
 
 /**
  * https://help.imgur.com/hc/en-us/articles/115000083326-What-files-can-I-upload-Is-there-a-size-limit-
