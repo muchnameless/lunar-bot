1d4499973509133affb02e1ef4b8f64ec24b5d1a
muchnameless <muchnameless@gmail.com>
2022-10-06 15:28:04 +0200

    chore(deps): lockfile

--- scripts/regen_lockfile.sh
+++ scripts/regen_lockfile.sh
@@ -1,6 +1,8 @@
 #!/bin/tcsh -f
 
 rm -rf node_modules
-rm yarn.lock
+
+# https://github.com/renovatebot/renovate/blob/c481ad235c2f18e37845d2541f8b7ed13ce19b90/lib/modules/manager/npm/post-update/yarn.ts#L252-L271
+echo '' > yarn.lock
 
 yarn
