50e1fbb2ece06fa0e4b8d57ea307cf6386b6f4ab
muchnameless <muchnameless@gmail.com>
2022-11-05 13:12:44 +0100

    chore(scripts): post start

---
+++ scripts/post_start.sh
@@ -0,0 +1,4 @@
+#!/bin/tcsh -f
+
+# save current commit
+git rev-parse --short HEAD > running.log
--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -5,6 +5,3 @@ if ( ! -f './dist/index.js' ) then
 	echo "compiling to js"
 	\time -f 'Done in %E, CPU Usage %P' yarn build
 endif
-
-# save current commit
-git rev-parse --short HEAD > running.log
