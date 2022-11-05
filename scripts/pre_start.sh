89206aaf37e031a9bf594b8b9354f804c24e46dc
muchnameless <muchnameless@gmail.com>
2022-11-05 12:19:56 +0100

    fix: script

--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -1,7 +1,7 @@
 #!/bin/tcsh -f
 
 # build if dist is missing
-if ( ! -d './dist/index.js' ) then
+if ( ! -f './dist/index.js' ) then
 	echo "compiling to js"
 	\time -f 'Done in %E, CPU Usage %P' yarn build
 endif
