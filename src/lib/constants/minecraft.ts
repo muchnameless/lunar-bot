201c4a9cc5abd85ea7239d31e7d792a771fb6945
muchnameless <muchnameless@gmail.com>
2022-11-14 23:00:26 +0100

    fix: mc version

--- src/lib/constants/minecraft.ts
+++ src/lib/constants/minecraft.ts
@@ -7,7 +7,7 @@ export const MAX_IGN_INPUT_LENGTH = Math.max(MAX_IGN_LENGTH, MAX_UUID_LENGTH);
 /**
  * mc client version
  */
-export const MC_CLIENT_VERSION = '1.19.2';
+export const MC_CLIENT_VERSION = '1.19';
 
 /**
  * display names of vanilla mc items and blocks
