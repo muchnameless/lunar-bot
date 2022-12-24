386ee023988f74a3de33514d0179a8def0b7aa3f
muchnameless <muchnameless@gmail.com>
2022-12-24 15:59:02 +0100

    chore(scripts): simplify

--- scripts/rebase_prs.sh
+++ scripts/rebase_prs.sh
@@ -1,6 +1,6 @@
 #!/bin/tcsh -f
 
-foreach b (`gh pr list --json headRefName | jq -r '.[] | .headRefName' | awk '! /renovate/ {print $0}'`)
+foreach b (`gh pr list --json headRefName --jq '.[].headRefName' | awk '! /renovate/ {print}'`)
     git checkout $b && git rebase main && git push -f
 end
 
