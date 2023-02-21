c689e21d001a33044af48a2ef6245be3b19b80a1
muchnameless <muchnameless@gmail.com>
2023-02-21 20:14:06 +0100

    fix(networth): pet xp fallback

--- src/lib/networth/functions/pets.ts
+++ src/lib/networth/functions/pets.ts
@@ -1,15 +1,25 @@
 import { type Components } from '@zikeji/hypixel';
 import { PET_XP_TOTAL } from '../constants/pets.js';
+import { logger } from '#logger';
 
 /**
  * @param pet
  */
 export function calculatePetSkillLevel(pet: Components.Schemas.SkyBlockProfilePet) {
+	// pet.tier is not affected by tier boost
 	const totalXp =
 		PET_XP_TOTAL[pet.type as keyof typeof PET_XP_TOTAL] ?? PET_XP_TOTAL[pet.tier as keyof typeof PET_XP_TOTAL];
 
+	if (totalXp) {
+		return {
+			level: totalXp.findLastIndex((requiredXp) => requiredXp <= pet.exp),
+			maxXP: totalXp.at(-1)!,
+		};
+	}
+
+	logger.warn({ pet }, '[CALCULATE PET SKILL LEVEL]: unknown type and tier');
 	return {
-		level: totalXp.findLastIndex((requiredXp) => requiredXp <= pet.exp),
-		maxXP: totalXp.at(-1)!,
+		level: 0,
+		maxXP: 0,
 	};
 }
